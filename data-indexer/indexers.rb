f9d362ed235912e24c03728af58b9efa0a092690
Shashank Teotia <shashankteotia@gmail.com>
2015-05-14 14:19:37 +0530

    [Shashank] modifying mapping as per telemetry API doc

--- data-indexer/indexers.rb
+++ data-indexer/indexers.rb
@@ -28,7 +28,7 @@ module Indexers
             },
             {
               double_fields: {
-                  match: "mem|idisk|edisk|scrn|length|age",
+                  match: "mem|idisk|edisk|scrn|length|exlength|age|percent_correct|percent_attempt|size",
                   match_pattern: "regex",
                   mapping: {
                       type: "double",
@@ -50,7 +50,7 @@ module Indexers
             },
             {
               integer_fields: {
-                  match: "sims|score|maxscore",
+                  match: "sims|score|maxscore|atmpts|failedatmpts|correct|incorrect|total",
                   match_pattern: "regex",
                   mapping: {
                       type: "integer",
@@ -101,28 +101,7 @@ module Indexers
             }
           }
         }
-    attr_reader :client
-    def initialize(refresh=true)
-      @client = ::Elasticsearch::Client.new log: false
-      if refresh
-        delete_indices
-        create_indices
-      end
-    end
-    def delete_indices
-      client.indices.delete_template name: 'ecosystem' rescue nil
-      # @client.indices.delete index: 'identities' rescue nil
-    end
-    def create_indices
-      client.indices.put_template({
-        name: "ecosystem",
-        body: {
-        order: 20,
-        template: "dump",
-        settings: {
-          # "index.refresh_interval": "5s"
-        },
-        mappings: {
+    DUMP_MAPPINGS = {
           _default_: {
             dynamic_templates: [
             {
@@ -162,11 +141,32 @@ module Indexers
               enabled: true
             }
           }
+        }
+    attr_reader :client
+    def initialize(refresh=true)
+      @client = ::Elasticsearch::Client.new log: false
+      if refresh
+        delete_templates
+        create_templates
+      end
+    end
+    def delete_templates
+      client.indices.delete_template name: 'ecosystem' rescue nil
+    end
+    def create_templates
+      puts client.indices.put_template({
+        name: "ecosystem",
+        body: {
+        order: 20,
+        template: "dump",
+        settings: {
+          # "index.refresh_interval": "5s"
         },
+        mappings: DUMP_MAPPINGS,
         aliases: {}
         }
       })
-      client.indices.put_template({
+      puts client.indices.put_template({
         name: "ecosystem",
         body: {
         order: 10,
@@ -178,181 +178,6 @@ module Indexers
         aliases: {}
         }
       })
-      # client.indices.put_template({
-      #   name: 'events',
-      #   body: {
-      #     template: '*',
-      #     settings: {
-      #       'index.number_of_shards' => 1
-      #     },
-      #     mappings: {
-      #       game_events_v1:{
-      #         properties: {
-      #           eid: { type: 'string', index: 'not_analyzed'},
-      #           did: { type: 'string', index: 'not_analyzed'},
-      #           sid: { type: 'string', index: 'not_analyzed'},
-      #           lang: { type: 'string', index: 'not_analyzed'},
-      #           pmode: { type: 'string', index: 'not_analyzed'},
-      #           gdata: {
-      #             type: 'nested',
-      #             include_in_parent: true,
-      #             properties: {
-      #               id: { type: 'string' },
-      #               ver: { type: 'string' }
-      #             }
-      #           },
-      #           edata: {
-      #             type: 'nested',
-      #             include_in_parent: true,
-      #             properties: {
-      #               eks: {
-      #                 type: 'nested',
-      #                 include_in_parent: true,
-      #                 properties: {
-      #                   length: { type: 'double'},
-      #                   score: { type: 'double' },
-      #                   correct: { type: 'long' },
-      #                   incorrect: { type: 'long' },
-      #                   percent_correct: { type: 'double' },
-      #                   result: { type: 'string', index: 'not_analyzed'},
-      #                   total: { type: 'long' }
-      #                 }
-      #               }
-      #             }
-      #           }
-      #         }
-      #       },
-      #       devices_v1: {
-      #         _id: {
-      #           path: "did"
-      #         },
-      #         properties: {
-      #           ts: { type: 'date' },
-      #           tu: { type: 'date' },
-      #           did: { type: 'string', index: 'not_analyzed'},
-      #           loc: { type: 'geo_point'},
-      #           ldata: {
-      #             type: 'nested',
-      #             include_in_parent: true,
-      #             properties: {
-      #               locality: { type: 'string', index: 'not_analyzed' },
-      #               district: { type: 'string', index: 'not_analyzed' },
-      #               state: { type: 'string', index: 'not_analyzed' },
-      #               country: { type: 'string', index: 'not_analyzed' }
-      #             }
-      #           },
-      #           dspec: {
-      #             type: 'nested',
-      #             include_in_parent: true,
-      #             properties: {
-      #               id: { type: 'string', index: 'not_analyzed' },
-      #               os: { type: 'string', index: 'not_analyzed' },
-      #               make: { type: 'string', index: 'not_analyzed' },
-      #               mem: { type: 'double' , index: 'not_analyzed' },
-      #               idisk: { type: 'double' },
-      #               edisk: { type: 'double' },
-      #               scrn: { type: 'double' },
-      #               camera: { type: 'string', index: 'not_analyzed' },
-      #               cpu: { type: 'string', index: 'not_analyzed' },
-      #               sims: { type: 'double' },
-      #               cap: { type: 'string', index: 'not_analyzed' },
-      #             }
-      #           }
-      #         }
-      #       },
-      #       events_v1: {
-      #         # dynamic: "false",
-      #         properties: {
-      #           eid: { type: 'string', index: 'not_analyzed', fielddata: { format: "doc_values" }},
-      #           ts: { type: 'date' },
-      #           ver: { type: 'string', index: 'not_analyzed'},
-      #           gdata: {
-      #             type: 'nested',
-      #             include_in_parent: true,
-      #             properties: {
-      #               id: { type: 'string' },
-      #               ver: { type: 'string' }
-      #             }
-      #           },
-      #           sid: { type: 'string', index: 'not_analyzed'},
-      #           uid: { type: 'string', index: 'not_analyzed'},
-      #           did: { type: 'string', index: 'not_analyzed'},
-      #           edata: {
-      #             type: 'nested',
-      #             include_in_parent: true,
-      #             properties: {
-      #               eks: {
-      #                 type: 'nested',
-      #                 include_in_parent: true,
-      #                 properties: {
-      #                   length: { type: 'double'},
-      #                   loc: { type: 'geo_point'},
-      #                   ldata: {
-      #                     type: 'nested',
-      #                     include_in_parent: true,
-      #                     properties: {
-      #                       locality: { type: 'string', index: 'not_analyzed' },
-      #                       district: { type: 'string', index: 'not_analyzed' },
-      #                       state: { type: 'string', index: 'not_analyzed' },
-      #                       country: { type: 'string', index: 'not_analyzed' }
-      #                     }
-      #                   },
-      #                   uid: { type: 'string', index: 'not_analyzed' },
-      #                   err: { type: 'string' },
-      #                   gid: { type: 'string', index: 'not_analyzed' },
-      #                   attrs: { type: 'string' },
-      #                   mode: { type: 'string', index: 'not_analyzed' },
-      #                   api: { type: 'string', index: 'not_analyzed' },
-      #                   msgid: { type: 'string', index: 'not_analyzed' },
-      #                   ver: { type: 'string', index: 'not_analyzed' },
-      #                   size: { type: 'double' },
-      #                   dspec: {
-      #                     type: 'nested',
-      #                     include_in_parent: true,
-      #                     properties: {
-      #                       os: { type: 'string', index: 'not_analyzed' },
-      #                       make: { type: 'string', index: 'not_analyzed' },
-      #                       mem: { type: 'double' , index: 'not_analyzed' },
-      #                       idisk: { type: 'double' },
-      #                       edisk: { type: 'double' },
-      #                       scrn: { type: 'double' },
-      #                       camera: { type: 'string', index: 'not_analyzed' },
-      #                       cpu: { type: 'string', index: 'not_analyzed' },
-      #                       sims: { type: 'double' },
-      #                       cap: { type: 'string', index: 'not_analyzed' },
-      #                     }
-      #                   },
-      #                   subj: { type: 'string', index: 'not_analyzed' },
-      #                   mc: { type: 'string', index: 'not_analyzed' },
-      #                   skill: { type: 'string', index: 'not_analyzed' },
-      #                   pass: { type: 'string', index: 'not_analyzed' },
-      #                   res: { type: 'string', index: 'not_analyzed' },
-      #                   exres: { type: 'string', index: 'not_analyzed' },
-      #                   uri: { type: 'string', index: 'not_analyzed' },
-      #                   topics: {
-      #                     type: 'nested',
-      #                     include_in_parent: true,
-      #                     properties: {
-      #                       mc: { type: 'string', index: 'not_analyzed' },
-      #                       skill: { type: 'string', index: 'not_analyzed' },
-      #                       methods: { type: 'string', index: 'not_analyzed' }
-      #                     }
-      #                   },
-      #                   type: {type: 'string', index: 'not_analyzed'},
-      #                   extype: {type: 'string', index: 'not_analyzed'},
-      #                 }
-      #               },
-      #               ext: { type: 'nested' }
-      #             }
-      #           }
-      #         }
-      #       }
-      #     }
-      #   }
-      # })
-      # @client.indices.create({
-      #   index: 'ecosystem-identities'
-      # })
     end
     def get(index,type,id)
       begin
