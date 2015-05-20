e92457059797483e9b18494ef1580b403ea0f253
Nitin Dhall <nitind@Nitins-MacBook-Air.local>
2015-05-20 18:01:26 +0530

    added device count tests

---
+++ features/steps/common_steps/device.rb
@@ -0,0 +1,24 @@
+module CommonSteps
+  module Device
+    include Spinach::DSL
+
+    step 'total device count is one' do
+      q = search({index:'test*',body: {
+        "query"=> {
+          "term"=>{
+            "eid"=> "GE_SESSION_START"
+          }
+        },
+        "size"=> 0,
+        "aggs"=> {
+          "unique_devices"=> {
+            "cardinality"=> {
+              "field"=> "did"
+            }
+          }
+        }
+      }})
+      q.aggregations.unique_devices.value.should == 1
+    end
+  end
+end
--- features/steps/test_how_device_count_works.rb
+++ features/steps/test_how_device_count_works.rb
@@ -1,8 +1,8 @@
-
-
 class Spinach::Features::TestHowDeviceCountWorks < Spinach::FeatureSteps
   include CommonSteps::ElasticsearchClient
   include CommonSteps::UserSimulation
+  include CommonSteps::Device
+  include CommonSteps::GamePlaySimulation
 
   THAT_TIME = Time.now
 
@@ -12,31 +12,88 @@ class Spinach::Features::TestHowDeviceCountWorks < Spinach::FeatureSteps
     create_templates["acknowledged"].should == true
   end
 
-  step 'I play for the first time on this device' do
+  step 'a new device will be added' do
+    total_device_count_is_one
+  end
+
+  step 'the device has already been used' do
+    the_device_has_never_been_used
+    i_play_on_this_device
+  end
+
+  step 'I play on this device on the same day' do
     t1 = THAT_TIME
     session = ::Generator::Session.new(user,device,t1)
     write_events(session,t1)
     q = search({q:"eid:GE_SESSION_START"})
-    q.hits.total.should == 1
+    q.hits.total.should == 2
   end
 
-  step 'a new device will be added' do
-    q = search({index:'test*',body: {
-      "query"=> {
-        "term"=>{
-          "eid"=> "GE_SESSION_START"
-        }
-      },
-      "size"=> 0,
-      "aggs"=> {
-        "unique_devices"=> {
-          "cardinality"=> {
-            "field"=> "did"
-          }
-        }
-      }
-    }})
-    q.aggregations.unique_devices.value.should == 1
+  step 'the number of devices does not change' do
+    total_device_count_is_one
   end
 
+  step 'I play on this device from a different location' do
+    t1 = THAT_TIME
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device(loc),t1)
+    write_events(session,t1)
+    q = search({q:"eid:GE_SESSION_START"})
+    q.hits.total.should == 2
+  end
+
+  step 'I play on this device on a different day' do
+    t1 = THAT_TIME+86400
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    q = search({q:"eid:GE_SESSION_START"})
+    q.hits.total.should == 2
+  end
+
+  step 'I play on this device on a different day at a different location' do
+    t1 = THAT_TIME+86400
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device(loc),t1)
+    write_events(session,t1)
+    q = search({q:"eid:GE_SESSION_START"})
+    q.hits.total.should == 2
+  end
+
+  step 'all of us play on this device on different days and at different locations' do
+    t1 = THAT_TIME+86400
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    t1 = THAT_TIME
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    t1 = THAT_TIME+190000
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    q = search({q:"eid:GE_SESSION_START"})
+    q.hits.total.should == 4
+  end
+
+  step 'all of us including new children play on this device' do
+    t1 = THAT_TIME+86400
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    t1 = THAT_TIME
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    @user ||= ::Generator::User.new
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    t1 = THAT_TIME+190000
+    @user ||= ::Generator::User.new
+    loc = "#{rand(12.0..20.0)},#{rand(74.0..78.0)}"
+    session = ::Generator::Session.new(user,device,t1)
+    write_events(session,t1)
+    q = search({q:"eid:GE_SESSION_START"})
+    q.hits.total.should == 5
+
+  end
 end
