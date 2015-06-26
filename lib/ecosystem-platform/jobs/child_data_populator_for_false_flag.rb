2cff45875c6155db6cec9df98c8d62eb73a50242
Nitin Dahall <nitind@sahajsoft.com>
2015-06-26 15:02:53 +0530

    [Singhania] Changing flag value to false adding other script to re-poulate when flag is false

--- lib/ecosystem-platform/jobs/child_data_populator_for_false_flag.rb
+++ lib/ecosystem-platform/jobs/child_data_populator_for_false_flag.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'hashie'
 require 'elasticsearch'
 require 'mysql2'
@@ -8,8 +7,8 @@ require_relative '../utils/ep_logging.rb'
 
 module EcosystemPlatform
   module Jobs
-    class ChildFlagReseter
-      PROGNAME = 'child_flag_reseter.jobs.ep'
+    class ChildDataPopulatorForFalseFlag
+      PROGNAME = 'child_data_populator_for_false_flag.jobs.ep'
       include EcosystemPlatform::Utils::EPLogging
 
       def self.perform(index="ecosystem-*")
@@ -20,7 +19,7 @@ module EcosystemPlatform
           # will be replaced by a config module
           @client = ::Elasticsearch::Client.new(host:ENV['ES_HOST']||'localhost',log: false)
           @client.indices.refresh index: index
-          logger.info("SEARCHING EVENTS TO RESET FLAG")
+          logger.info("SEARCHING EVENTS TO UPDATE DATA FOR FALSE FLAG")
           response = @client.search({
             index: index,
             type: 'events_v1',
@@ -48,12 +47,14 @@ module EcosystemPlatform
                         },
                         {
                           "exists" => {
-                            "field" => "udata.age"
+                            "field" => "flags.child_data_processed"
                           }
                         },
                         {
-                          "exists" => {
-                            "field" => "flags.child_data_processed"
+                          "filter" => {
+                            "term" => {
+                              "flags.child_data_processed" => false
+                            }
                           }
                         }
                       ]
@@ -67,11 +68,13 @@ module EcosystemPlatform
           logger.info "FOUND #{response.hits.total} hits."
           response.hits.hits.each do |hit|
             uid = hit._source.uid
+            results = @db_client.query("SELECT * FROM children where encoded_id = '#{uid}'")
+            child = Hashie::Mash.new results.first
             result = @client.update({
               index: hit._index,
               type: hit._type,
               id: hit._id,
-              body: {doc: {flags: {child_data_processed: false}}}
+              body: request_body(child,hit)
             })
             logger.info "<- RESULT #{result}"
           end
@@ -80,6 +83,32 @@ module EcosystemPlatform
           logger.error(e,{backtrace: e.backtrace[0..4]})
           logger.end_task
         end
+      end
+
+      def self.request_body(child,hit)
+        data = Hash.new
+        doc = {doc: {flags: {child_data_processed: true}}}
+	  	begin
+        	if(child.dob)
+          		time_then = Time.strptime(hit._source.ts,'%Y-%m-%dT%H:%M:%S%z')
+          		age_then = time_then - child.dob
+          		seconds_in_a_year = 31556952
+          		completed_years = (age_then / seconds_in_a_year).floor
+          		data[:age]= age_then
+          		data[:dob]= child.dob
+          		data[:age_completed_years]= completed_years
+          		logger.info "-> UPDATE #{hit._source.eid} #{hit._id}"
+        	end
+        	data[:gender]=child.gender if child.gender
+          data[:ekstep_id] = child.ekstep_id if child.ekstep_id
+          data[:name] = child.name if child.name
+        	doc[:doc][:udata]=data unless data.empty?
+		rescue => e
+			logger.error(e,{backtrace: e.backtrace[0..4]})
+			logger.end_task
+		ensure
+			return doc
+		end
       end
     end
   end
