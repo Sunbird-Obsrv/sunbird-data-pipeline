0259b550d0d31b992df5e21e4f4e566b2c57deae
sreeharikmarar <sreeharikmarar@gmail.com>
2016-02-10 13:57:26 +0530

    [sreehari] deleted all the commented cron jobs

--- tasks/config/schedule.rb
+++ tasks/config/schedule.rb
@@ -7,45 +7,9 @@
 #
 set :output, {:error => 'error.log', :standard => 'cron.log'}
 
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
 set :environment_variable, 'EP_LOG_DIR'
 set :environment, ENV['EP_LOG_DIR']
-
-# every 1.minute do
-#   rake "scheduled:reverse_search"
-# end
-# every 1.minute do
-#   rake "scheduled:set_ldata"
-# end
-# DISABLED because devices are generating signup events
-# every 1.minute do
-#   rake "scheduled:generate_signup"
-# end
-# every 1.minute do
-#   rake "scheduled:oe_summarize"
-# end
-# every 1.minute do
-#   rake "scheduled:child_data_populator"
-# end
-
-# every 1.hour do
-#   rake "scheduled:decrypt_partner"
-# end
-
-# every 2.hour do
-#   rake "scheduled:denormalize_partner"
-# end
 
 every 6.hour do
   rake "scheduled:handle_denormalizer"
