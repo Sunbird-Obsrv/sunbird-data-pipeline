bcc882182af24584315bf09f90835db18ccebde1
Shashank Teotia <shashankteotia@gmail.com>
2016-04-21 17:30:06 +0530

    [Gaurav] #227 checking for valid Partner

--- data-pipeline/partner-data-router/src/main/java/org/ekstep/ep/samza/Event.java
+++ data-pipeline/partner-data-router/src/main/java/org/ekstep/ep/samza/Event.java
@@ -4,10 +4,14 @@ import org.apache.commons.lang.StringUtils;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class Event {
+    private static final List<String> validPartners = Arrays.asList("org.ekstep.partner.akshara", "org.ekstep.partner.pratham");
     private Map<String, Object> data;
+
 
     public Event(Map<String, Object> data) {
         this.data = data;
@@ -33,8 +37,11 @@ public class Event {
             return null;
         String partnerid = "partnerid";
         for (Map tag:tags)
-            if(tag!=null && tag.containsKey(partnerid))
-                return (String) tag.get(partnerid);
+            if(tag!=null && tag.containsKey(partnerid)) {
+                String partnerId = (String) tag.get(partnerid);
+                if(validPartners.contains(partnerId))
+                    return partnerId;
+            }
         return null;
     }
 
