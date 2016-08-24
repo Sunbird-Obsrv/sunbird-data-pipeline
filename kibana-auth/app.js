69ba71e6961bc492b904f4585e0d65d8ef034ccd
sreeharikmarar <sreeharikmarar@gmail.com>
2016-08-24 12:48:45 +0530

    #326 | kiban auth bin file chages

--- kibana-auth/app.js
+++ kibana-auth/app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'kibana.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'kibana.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
