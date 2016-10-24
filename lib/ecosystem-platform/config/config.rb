9e97a7c40b331805d4d8293f25183619d64f592c
gauravsinghania <grvsinghania@gmail.com>
2016-10-24 17:07:48 +0530

    ekstep/AWS-Setup#64 | Changing config file name | <Singhania>

--- lib/ecosystem-platform/config/config.rb
+++ lib/ecosystem-platform/config/config.rb
@@ -2,7 +2,7 @@ require 'hashie'
 
 class Config
   def self.load
-    config = YAML.load_file(File.join(__dir__, 'config.yml'))
+    config = YAML.load_file(File.join(__dir__, 'public.yml'))
     return Hashie::Mash.new config
   end
   def self.load_partner_spec(partner)
