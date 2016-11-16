e280d8533af65117384e421dfbd157a892197a02
sreeharikmarar <sreeharikmarar@gmail.com>
2016-11-16 13:57:02 +0530

    #376 | ruby file archive job modifications to skip the current hour log file

--- lib/ecosystem-platform/jobs/file_archiver.rb
+++ lib/ecosystem-platform/jobs/file_archiver.rb
@@ -36,7 +36,7 @@ module EcosystemPlatform
         # Find all files matching pattern /var/log/partners/**/*.log
         all_log_files = Dir.glob(File.join(src_dir, "**/*.log"))
         # E.g. current_hour_log_regex_pattern: /var/log/partners/partner-name/2016/01/31/14/data-exhaust-2016-01-31-14.log
-        current_hour_log_regex_pattern = /#{Regexp.escape(src_dir)+"[0-9a-z]+" + Regexp.escape("/#{current_time.year}/#{current_time.strftime("%m")}/#{current_time.strftime("%d")}/#{current_time.strftime("%H")}/data-exhaust-#{current_time.year}-#{current_time.strftime("%m")}-#{current_time.strftime("%d")}-#{current_time.strftime("%H")}.log")}/
+        current_hour_log_regex_pattern = /#{Regexp.escape(src_dir)+ "/" +"[0-9a-z]+" + Regexp.escape("/#{current_time.year}/#{current_time.strftime("%m")}/#{current_time.strftime("%d")}/#{current_time.strftime("%H")}/data-exhaust-#{current_time.year}-#{current_time.strftime("%m")}-#{current_time.strftime("%d")}-#{current_time.strftime("%H")}.log")}/
         return all_log_files.reject { |f| current_hour_log_regex_pattern.match(f) }
       end
 
