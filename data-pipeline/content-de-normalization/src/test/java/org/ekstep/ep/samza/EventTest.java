592acdcc5eb14b59e97543ce03f24b8dc080827d
sreeharikmarar <sreeharikmarar@gmail.com>
2017-04-26 15:03:53 +0530

    Issue #493 feat : updating tests

--- data-pipeline/content-de-normalization/src/test/java/org/ekstep/ep/samza/EventTest.java
+++ data-pipeline/content-de-normalization/src/test/java/org/ekstep/ep/samza/EventTest.java
@@ -1,117 +1,79 @@
 package org.ekstep.ep.samza;
 
-import org.apache.samza.config.Config;
 import org.ekstep.ep.samza.domain.Event;
 import org.ekstep.ep.samza.fixture.EventFixture;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class EventTest {
 
-    HashMap<String, Object> contentEventMap = new HashMap<String,Object>();
+    HashMap<String, Object> contentTaxonomy = new HashMap<String,Object>();
 
     @Before
     public void setUp() {
-        createContentEventMap();
+        createContentTaxonomy();
     }
 
     @Test
     public void testGetContentIdOfGeLaunchEvent() throws Exception {
-        Event e = new Event(EventFixture.GeLaunchEvent(),contentEventMap);
+        Event e = new Event(EventFixture.GeLaunchEvent(), contentTaxonomy);
 
         assertEquals("do_30076072", e.getContentId());
     }
 
     @Test
     public void testGetContentIdOfOeEvent() throws Exception {
-        Event e = new Event(EventFixture.OeEvent(),contentEventMap);
+        Event e = new Event(EventFixture.OeEvent(), contentTaxonomy);
 
         assertEquals("do_30076072", e.getContentId());
     }
 
     @Test
     public void testGetContentIdOfMeEvent() throws Exception {
-        Event e = new Event(EventFixture.MeEvent(),contentEventMap);
+        Event e = new Event(EventFixture.MeEvent(), contentTaxonomy);
 
         assertEquals("do_30076072", e.getContentId());
     }
 
     @Test
     public void testGetContentIdOfBeEvent() throws Exception {
-        Event e = new Event(EventFixture.BeEvent(),contentEventMap);
+        Event e = new Event(EventFixture.BeEvent(), contentTaxonomy);
 
         assertEquals("do_30076072", e.getContentId());
     }
 
     @Test
     public void testGetContentIdOfCpEvent() throws Exception {
-        Event e = new Event(EventFixture.CpEvent(),contentEventMap);
+        Event e = new Event(EventFixture.CpEvent(), contentTaxonomy);
 
         assertEquals("do_30076072", e.getContentId());
     }
 
     @Test
     public void testGetContentIdOfCeEvent() throws Exception {
-        Event e = new Event(EventFixture.CeEvent(),contentEventMap);
+        Event e = new Event(EventFixture.CeEvent(), contentTaxonomy);
 
         assertEquals("do_30076072", e.getContentId());
     }
 
     @Test
     public void testGetContentIdReturnsNullWhenItIsAbsent() throws Exception {
-        Event e = new Event(EventFixture.EventWithoutContentId(),contentEventMap);
+        Event e = new Event(EventFixture.EventWithoutContentId(), contentTaxonomy);
 
         assertEquals(null, e.getContentId());
     }
 
-    private  HashMap<String,Object> getContentFieldMap(HashMap<String,Object> eventTypes, HashMap<String,Object> fields) {
-
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        List<String> events = commaSeparatedStringToList(eventTypes, "gid.overridden.events");
-        for (String event : events) {
-            String eventType = event.split("\\.")[0];
-            List<String> gidPath = getGidPath(fields, event);
-            map.put(eventType.toUpperCase(), gidPath);
-        }
-        return map;
-    }
-
-    private List<String> commaSeparatedStringToList(HashMap<String,Object> config, String key) {
-        String[] split = ((String) config.get(key)).split(",");
-        List<String> list = new ArrayList<String>();
-        for (String event : split) {
-            list.add(event.trim());
-        }
-        return list;
-    }
-
-    private List<String> getGidPath(HashMap<String,Object> config, String key) {
-        String[] split = ((String) config.get(key)).split("\\.");
-        List<String> list = new ArrayList<String>();
-        for (String e : split) {
-            list.add(e.trim());
-        }
-        return list;
-    }
-
-    private void createContentEventMap() {
-        HashMap<String, Object> overRiddenEvents = new HashMap<String, Object>();
-        HashMap<String, Object> eventGidFields = new HashMap<String, Object>();
-        overRiddenEvents.put("gid.overridden.events","me.gid.field,ge.gid.field,oe.gid.field,ce.gid.field,cp.gid.field,be.gid.field");
-        eventGidFields.put("oe.gid.field","gdata.id");
-        eventGidFields.put("ge.gid.field","edata.eks.gid");
-        eventGidFields.put("me.gid.field","dimensions.content_id");
-        eventGidFields.put("ce.gid.field","context.content_id");
-        eventGidFields.put("cp.gid.field","edata.eks.action");
-        eventGidFields.put("be.gid.field","edata.eks.cid");
-
-        contentEventMap = getContentFieldMap(overRiddenEvents, eventGidFields);
+    private void createContentTaxonomy() {
+        contentTaxonomy.put("OE", asList("gdata", "id"));
+        contentTaxonomy.put("GE", asList("edata", "eks", "gid"));
+        contentTaxonomy.put("ME", asList("dimensions", "content_id"));
+        contentTaxonomy.put("CE", asList("context", "content_id"));
+        contentTaxonomy.put("CP", asList("edata", "eks", "action"));
+        contentTaxonomy.put("BE", asList("edata", "eks", "cid"));
     }
 }
