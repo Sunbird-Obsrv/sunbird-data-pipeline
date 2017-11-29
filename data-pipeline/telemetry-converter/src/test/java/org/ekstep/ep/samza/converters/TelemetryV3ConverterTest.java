f2e45d847fbce106db21ba18c1440a0672f2e9a3
Navaneeth K N <navaneethkn@gmail.com>
2017-11-29 14:59:09 +0530

    Issue #626 fix: TelemetryConverter is one -> many
    For one GE_INTERACT event, multiple v3 events will be generated

--- data-pipeline/telemetry-converter/src/test/java/org/ekstep/ep/samza/converters/TelemetryV3ConverterTest.java
+++ data-pipeline/telemetry-converter/src/test/java/org/ekstep/ep/samza/converters/TelemetryV3ConverterTest.java
@@ -7,15 +7,15 @@ import org.ekstep.ep.samza.reader.TelemetryReaderException;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
 public class TelemetryV3ConverterTest {
 
     @Test
-    public void convertEnvelope() throws FileNotFoundException, TelemetryReaderException {
+    public void convertEnvelope() throws Exception {
         Map<String, Object> oeStart = EventFixture.getEvent("OE_START");
         TelemetryV3Converter converter = new TelemetryV3Converter(oeStart);
         TelemetryV3[] v3 = converter.convert();
@@ -24,7 +24,7 @@ public class TelemetryV3ConverterTest {
         assertEquals(v3Map.get("eid"), "START");
         assertEquals(v3Map.get("ets"), 1510216719872L);
         assertEquals(v3Map.get("ver"), "3.0");
-        assertEquals(v3Map.get("mid"), "3f34adf0-89d5-4884-8920-4fadbe9680cd");
+        assertEquals(v3Map.get("mid"), "76953ec6ea4cf7724931289d9f0e0619787da753");
 
         Actor actor = (Actor) v3Map.get("actor");
         assertEquals("516a4365-eb22-44c0-add1-0b9000d1d09f", actor.getId());
@@ -47,7 +47,7 @@ public class TelemetryV3ConverterTest {
     }
 
 	 @Test
-	 public void convertOE_START() throws TelemetryReaderException, FileNotFoundException {
+	 public void convertOE_START() throws Exception {
          Map<String, Object> oeStart = EventFixture.getEvent("OE_START");
          TelemetryV3Converter converter = new TelemetryV3Converter(oeStart);
          TelemetryV3[] v3 = converter.convert();
@@ -61,7 +61,7 @@ public class TelemetryV3ConverterTest {
 	 }
 
     @Test
-    public void convertCE_START() throws TelemetryReaderException, FileNotFoundException {
+    public void convertCE_START() throws Exception {
         Map<String, Object> oeStart = EventFixture.getEvent("CE_START");
         TelemetryV3Converter converter = new TelemetryV3Converter(oeStart);
         TelemetryV3[] v3 = converter.convert();
@@ -78,7 +78,7 @@ public class TelemetryV3ConverterTest {
 
 
     @Test
-    public void convertCP_IMPRESSION() throws TelemetryReaderException, FileNotFoundException {
+    public void convertCP_IMPRESSION() throws Exception {
         Map<String, Object> cpImpression = EventFixture.getEvent("CP_IMPRESSION");
         TelemetryV3Converter converter = new TelemetryV3Converter(cpImpression);
         TelemetryV3[] v3 = converter.convert();
@@ -94,7 +94,7 @@ public class TelemetryV3ConverterTest {
     }
 
     @Test
-    public void convertCP_INTERACT() throws TelemetryReaderException, FileNotFoundException {
+    public void convertCP_INTERACT() throws Exception {
         Map<String, Object> cpInteraction = EventFixture.getEvent("CP_INTERACT");
         TelemetryV3Converter converter = new TelemetryV3Converter(cpInteraction);
         TelemetryV3[] v3 = converter.convert();
@@ -114,7 +114,7 @@ public class TelemetryV3ConverterTest {
     }
 
     @Test
-    public void convertCE_INTERACT() throws TelemetryReaderException, FileNotFoundException {
+    public void convertCE_INTERACT() throws Exception {
         Map<String, Object> ceInteract = EventFixture.getEvent("CE_INTERACT");
         TelemetryV3Converter converter = new TelemetryV3Converter(ceInteract);
         TelemetryV3[] v3 = converter.convert();
@@ -135,5 +135,94 @@ public class TelemetryV3ConverterTest {
         Plugin plugin = (Plugin) eData.get("plugin");
         assertEquals(plugin.getId(), "org.ekstep.ceheader");
         assertEquals(plugin.getVer(), "1.0");
+    }
+
+    @Test
+    public void convertGE_SESSION_START() throws Exception {
+        Map<String, Object> oeStart = EventFixture.getEvent("GE_SESSION_START");
+        TelemetryV3Converter converter = new TelemetryV3Converter(oeStart);
+        TelemetryV3[] v3 = converter.convert();
+        Map<String, Object> v3Map = v3[0].toMap();
+
+        Map<String, String> eData = (Map<String, String>) v3Map.get("edata");
+        assertEquals("", eData.get("loc"));
+        assertEquals(0, eData.get("duration"));
+        assertEquals("session", eData.get("type"));
+        assertEquals("", eData.get("pageid"));
+    }
+
+    @Test
+    public void convertGE_SESSION_END() throws Exception {
+        Map<String, Object> oeStart = EventFixture.getEvent("GE_SESSION_END");
+        TelemetryV3Converter converter = new TelemetryV3Converter(oeStart);
+        TelemetryV3[] v3 = converter.convert();
+        Map<String, Object> v3Map = v3[0].toMap();
+
+        Map<String, String> eData = (Map<String, String>) v3Map.get("edata");
+        assertEquals("", eData.get("mode"));
+        assertEquals(5438.0, eData.get("duration"));
+        assertEquals("session", eData.get("type"));
+    }
+
+    @Test
+    public void convertGE_INTERRUPT() throws Exception {
+        Map<String, Object> event = EventFixture.getEvent("GE_INTERRUPT");
+        TelemetryV3Converter converter = new TelemetryV3Converter(event);
+        TelemetryV3[] v3 = converter.convert();
+        Map<String, Object> v3Map = v3[0].toMap();
+
+        Map<String, String> eData = (Map<String, String>) v3Map.get("edata");
+        assertEquals("", eData.get("pageid"));
+        assertEquals("BACKGROUND", eData.get("type"));
+    }
+
+    @Test
+    public void convertGE_INTERACT() throws Exception {
+        Map<String, Object> event = EventFixture.getEvent("GE_INTERACT");
+        TelemetryV3Converter converter = new TelemetryV3Converter(event);
+        TelemetryV3[] v3 = converter.convert();
+        Map<String, Object> v3Map = v3[0].toMap();
+
+        assertEquals(1, v3.length);
+        assertEquals("INTERACT", v3[0].getEid());
+    }
+
+    @Test
+    public void convertGE_INTERACT_SUBTYPE_SHOW() throws Exception {
+        Map<String, Object> event = EventFixture.getEvent("GE_INTERACT_SUBTYPE_SHOW");
+        TelemetryV3Converter converter = new TelemetryV3Converter(event);
+        TelemetryV3[] v3 = converter.convert();
+
+        assertEquals(3, v3.length);
+        assertEquals(1, Arrays.stream(v3).filter(e -> "IMPRESSION".equals(e.getEid())).count());
+        assertEquals(1, Arrays.stream(v3).filter(e -> "LOG".equals(e.getEid())).count());
+        assertEquals(1, Arrays.stream(v3).filter(e -> "INTERACT".equals(e.getEid())).count());
+
+        TelemetryV3 impression = Arrays.stream(v3).filter(e -> "IMPRESSION".equals(e.getEid())).findFirst().get();
+        assertEquals(true, impression.getEdata().containsKey("visits"));
+        assertEquals("OTHER", impression.getEdata().get("type"));
+        assertEquals("Genie-TelemetrySync", impression.getEdata().get("pageid"));
+
+        TelemetryV3 log = Arrays.stream(v3).filter(e -> "LOG".equals(e.getEid())).findFirst().get();
+        assertEquals(true, log.getEdata().containsKey("params"));
+
+        TelemetryV3 interact = Arrays.stream(v3).filter(e -> "INTERACT".equals(e.getEid())).findFirst().get();
+        assertEquals(true, interact.getEdata().containsKey("plugin"));
+        assertEquals("show", interact.getEdata().get("subtype"));
+        assertEquals("Genie-TelemetrySync", interact.getEdata().get("pageid"));
+        assertEquals("OTHER", interact.getEdata().get("type"));
+    }
+
+    @Test
+    public void convertGE_INTERACT_SUBTYPE_SHOW_MID_Should_Be_Different() throws Exception {
+        Map<String, Object> event = EventFixture.getEvent("GE_INTERACT_SUBTYPE_SHOW");
+        TelemetryV3Converter converter = new TelemetryV3Converter(event);
+        TelemetryV3[] v3Events = converter.convert();
+        HashSet<String> uniqueMIDs = new HashSet<>();
+        for(TelemetryV3 v3 : v3Events) {
+            uniqueMIDs.add(v3.getMid());
+        }
+
+        assertEquals(3, uniqueMIDs.size());
     }
 }
