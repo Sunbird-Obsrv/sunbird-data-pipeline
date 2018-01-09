1e50cb65314c80fd0ff342b6945f360ff465f527
sreeharikmarar <sreeharikmarar@gmail.com>
2018-01-09 12:54:00 +0530

    Issue #644 feat: ruby script modifications

--- lib/ecosystem-platform/data_validators/pump_validator.rb
+++ lib/ecosystem-platform/data_validators/pump_validator.rb
@@ -23,6 +23,7 @@ module EcosystemPlatform
           logger.info("SEARCHING EVENTS TO VALIDATE")
           mid_found = 0
           mid_not_found = 0
+          mid_not_indexed = 0
           Dir.glob(file_path) do |file_name|
             logger.info "PROCESSING FILE : #{file_name}"
             File.foreach(file_name) do |mid|
@@ -44,23 +45,29 @@ module EcosystemPlatform
                 } 
               })
               response = Hashie::Mash.new response
-              logger.info "FOUND #{response.hits.hits.count} hits. - TOTAL #{response.hits.total}"
-              response.hits.hits.each do |hit|
-                metadata = hit._source.metadata
-                if(metadata != nil)
-                  if metadata.source_mid == mid
-                    logger.info "MID #{mid} IS MATCHING WITH METADATA SOURCE MID #{metadata.source_mid}"
-                    mid_found += 1
-                  else
-                    logger.info "#{mid} : MID NOT FOUND"
-                    mid_not_found += 1
+              if response.hits.total >= 1
+                logger.info "FOUND #{mid} - TOTAL #{response.hits.total} hits"
+                response.hits.hits.each do |hit|
+                  metadata = hit._source.metadata
+                  if(metadata != nil)
+                    if metadata.source_mid == mid
+                      logger.info "MID #{mid} IS MATCHING WITH METADATA SOURCE MID #{metadata.source_mid}"
+                      mid_found += 1
+                    else
+                      logger.info "#{mid} : MID NOT MATCHING"
+                      mid_not_found += 1
+                    end
                   end
                 end
+              else 
+                logger.info "#{mid} : MID NOT INDEXED"
+                mid_not_indexed += 1
               end
             end
           end
           logger.info "COUNT MID FOUND: #{mid_found}"
           logger.info "COUNT MID NOT FOUND: #{mid_not_found}"
+          logger.info "COUNT MID NOT INDEXED: #{mid_not_indexed}"
           logger.end_task
         rescue => e
           logger.error(e,{backtrace: e.backtrace[0..4]})
