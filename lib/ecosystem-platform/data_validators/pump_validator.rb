1c3751872f8cd60e9690c0d00deb5f3ebfbdac76
sreeharikmarar <sreeharikmarar@gmail.com>
2018-01-10 16:52:19 +0530

    Issue #644 feat: ruby script modifications

--- lib/ecosystem-platform/data_validators/pump_validator.rb
+++ lib/ecosystem-platform/data_validators/pump_validator.rb
@@ -21,13 +21,15 @@ module EcosystemPlatform
           @client = ::Elasticsearch::Client.new(host:ENV['ES_HOST']||'localhost',log: false)
           @client.indices.refresh index: index
           logger.info("SEARCHING EVENTS TO VALIDATE")
-          mid_found = 0
-          mid_not_found = 0
+          total_events = 0
+          mid_found_in_metadata = 0
+          mid_not_found_metadata = 0
           mid_not_indexed = 0
           Dir.glob(file_path) do |file_name|
             logger.info "PROCESSING FILE : #{file_name}"
             File.foreach(file_name) do |mid|
               mid = mid.strip
+              q_mid = mid.split(":").last
               response = @client.search({
                 index: index,
                 type: 'events_v1',
@@ -37,17 +39,17 @@ module EcosystemPlatform
                     "filtered": {
                       "query": {
                        "query_string": {
-                          "query": "mid: \"#{mid}\""
+                          "query": "mid: \"#{q_mid}\""
                         }
                       }
                     }
                   }
                 } 
               })
+              total_events += 1
               response = Hashie::Mash.new response
               logger.info "#{mid} - TOTAL #{response.hits.total} hits"
               if response.hits.total >= 1
-                logger.info "FOUND #{mid} "
                 response.hits.hits.each do |hit|
                   metadata = hit._source.metadata
                   if(metadata != nil)
@@ -69,6 +71,7 @@ module EcosystemPlatform
           logger.info "COUNT MID FOUND: #{mid_found}"
           logger.info "COUNT MID NOT FOUND: #{mid_not_found}"
           logger.info "COUNT MID NOT INDEXED: #{mid_not_indexed}"
+          logger.info "TOTAL COUNT : #{total_events}"
           logger.end_task
         rescue => e
           logger.error(e,{backtrace: e.backtrace[0..4]})
