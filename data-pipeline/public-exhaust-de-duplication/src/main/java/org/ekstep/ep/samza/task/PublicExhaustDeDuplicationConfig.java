953c0b32a6aeb35c924d4bbdad4ba7ae2f47815e
sreeharikmarar <sreeharikmarar@gmail.com>
2018-03-01 11:01:37 +0530

    Issue #658 feat : pipeline monitoring | public exhaust de duplication job changes

--- data-pipeline/public-exhaust-de-duplication/src/main/java/org/ekstep/ep/samza/task/PublicExhaustDeDuplicationConfig.java
+++ data-pipeline/public-exhaust-de-duplication/src/main/java/org/ekstep/ep/samza/task/PublicExhaustDeDuplicationConfig.java
@@ -8,7 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public class PublicExhaustDeDuplicationConfig {
-
+    private final String JOB_NAME = "PublicExhaustDeDuplication";
+    private final String metricsTopic;
     private String successTopic;
     private String failedTopic;
     private String duplicateTopic;
@@ -17,6 +18,7 @@ public class PublicExhaustDeDuplicationConfig {
         successTopic = config.get("output.success.topic.name", "telemetry.public_exhaust");
         failedTopic = config.get("output.failed.topic.name", "telemetry.public_exhaust.fail");
         duplicateTopic = config.get("output.duplicate.topic.name", "telemetry.public_exhaust.duplicate");
+        metricsTopic = config.get("output.metrics.topic.name", "pipeline_metrics");
     }
 
     public String successTopic() {
@@ -29,5 +31,13 @@ public class PublicExhaustDeDuplicationConfig {
 
     public String duplicateTopic() {
         return duplicateTopic;
+    }
+
+    public String metricsTopic() {
+        return metricsTopic;
+    }
+
+    public String jobName() {
+        return JOB_NAME;
     }
 }
