5d91bf196e86508eef16b238465ad1f70a888435
Santhosh <santhosh@ilimi.in>
2018-07-26 16:07:00 +0530

    Issue #SB-5047 fix: Fix compilation issues

---
+++ data-pipeline/ep-core/src/main/java/org/ekstep/ep/samza/core/BaseSink.java
@@ -0,0 +1,24 @@
+package org.ekstep.ep.samza.core;
+
+import java.util.Map;
+
+import org.apache.samza.system.OutgoingMessageEnvelope;
+import org.apache.samza.system.SystemStream;
+import org.apache.samza.task.MessageCollector;
+
+public class BaseSink {
+
+	private MessageCollector collector;
+	
+	public BaseSink(MessageCollector collector) {
+		this.collector = collector;
+	}
+	
+	public void toTopic(String topic, String mid, String message) {
+		collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", topic), mid, message));
+	}
+	
+	public void toTopic(String topic, String mid, Map<String, Object> message) {
+		collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", topic), mid, message));
+	}
+}
