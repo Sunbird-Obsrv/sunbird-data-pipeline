292320fd8494f2f4d233941deacff97962441ba1
Anand <anandp@ilimi.in>
2018-10-17 23:40:06 +0530

    Issue #SB-7256 fix: Handle exceptions from Validator for message size

--- data-pipeline/telemetry-validator/src/main/java/org/ekstep/ep/samza/task/TelemetryValidatorSink.java
+++ data-pipeline/telemetry-validator/src/main/java/org/ekstep/ep/samza/task/TelemetryValidatorSink.java
@@ -3,12 +3,16 @@ package org.ekstep.ep.samza.task;
 import org.apache.samza.task.MessageCollector;
 import org.ekstep.ep.samza.core.BaseSink;
 import org.ekstep.ep.samza.core.JobMetrics;
+import org.ekstep.ep.samza.core.Logger;
 import org.ekstep.ep.samza.domain.Event;
+
+import static java.text.MessageFormat.format;
 
 public class TelemetryValidatorSink extends BaseSink {
     
     private JobMetrics metrics;
     private TelemetryValidatorConfig config;
+    static Logger LOGGER = new Logger(TelemetryValidatorSink.class);
 
     public TelemetryValidatorSink(MessageCollector collector, JobMetrics metrics,
                                   TelemetryValidatorConfig config) {
@@ -25,13 +29,25 @@ public class TelemetryValidatorSink extends BaseSink {
 
     public void toFailedTopic(Event event, String failedMessage) {
     	event.markFailure(failedMessage, config);
-        toTopic(config.failedTopic(), event.mid(), event.getJson());
+        try {
+            toTopic(config.failedTopic(), event.mid(), event.getJson());
+        } catch (Exception ex) {
+            LOGGER.error(null, format(
+                    "EXCEPTION. PASSING EVENT MID {0} THROUGH FROM FAILED TOPIC. EXCEPTION: ",
+                    event.mid()), ex.getMessage());
+        }
         metrics.incFailedCounter();
     }
 
     public void toErrorTopic(Event event, String errorMessage) {
     	event.markFailure(errorMessage, config);
-        toTopic(config.failedTopic(), event.mid(), event.getJson());
+    	try {
+            toTopic(config.failedTopic(), event.mid(), event.getJson());
+        } catch (Exception ex) {
+            LOGGER.error(null, format(
+                    "EXCEPTION. PASSING EVENT MID {0} THROUGH FROM FAILED TOPIC. EXCEPTION: ",
+                    event.mid()), ex.getMessage());
+        }
         metrics.incErrorCounter();
     }
 
