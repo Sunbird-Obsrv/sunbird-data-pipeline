6dafdbee8e1269c0a0f115b5348e8456416c2f14
Anand <anandp@ilimi.in>
2018-10-18 00:17:01 +0530

    Issue #SB-7256 fix: Add exception handling to BaseSink

--- data-pipeline/ep-core/src/main/java/org/ekstep/ep/samza/core/BaseSink.java
+++ data-pipeline/ep-core/src/main/java/org/ekstep/ep/samza/core/BaseSink.java
@@ -6,19 +6,32 @@ import org.apache.samza.system.OutgoingMessageEnvelope;
 import org.apache.samza.system.SystemStream;
 import org.apache.samza.task.MessageCollector;
 
+import static java.text.MessageFormat.format;
+
 public class BaseSink {
 
 	private MessageCollector collector;
+	private static Logger LOGGER = new Logger(BaseSink.class);
 	
 	public BaseSink(MessageCollector collector) {
 		this.collector = collector;
 	}
 	
 	public void toTopic(String topic, String mid, String message) {
-		collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", topic), mid, message));
+		try {
+			collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", topic), mid, message));
+		} catch (Exception ex) {
+			LOGGER.error(null, format(
+					"PASSING EVENT MID {0} THROUGH FROM {1} TOPIC. EXCEPTION: ", mid, topic), ex.getMessage());
+		}
 	}
 	
 	public void toTopic(String topic, String mid, Map<String, Object> message) {
-		collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", topic), mid, message));
+		try {
+			collector.send(new OutgoingMessageEnvelope(new SystemStream("kafka", topic), mid, message));
+		} catch (Exception ex) {
+			LOGGER.error(null, format(
+					"PASSING EVENT MID {0} THROUGH FROM {1} TOPIC. EXCEPTION: ", mid, topic), ex.getMessage());
+		}
 	}
 }
--- data-pipeline/telemetry-validator/src/main/java/org/ekstep/ep/samza/task/TelemetryValidatorSink.java
+++ data-pipeline/telemetry-validator/src/main/java/org/ekstep/ep/samza/task/TelemetryValidatorSink.java
@@ -3,16 +3,12 @@ package org.ekstep.ep.samza.task;
 import org.apache.samza.task.MessageCollector;
 import org.ekstep.ep.samza.core.BaseSink;
 import org.ekstep.ep.samza.core.JobMetrics;
-import org.ekstep.ep.samza.core.Logger;
 import org.ekstep.ep.samza.domain.Event;
-
-import static java.text.MessageFormat.format;
 
 public class TelemetryValidatorSink extends BaseSink {
     
     private JobMetrics metrics;
     private TelemetryValidatorConfig config;
-    static Logger LOGGER = new Logger(TelemetryValidatorSink.class);
 
     public TelemetryValidatorSink(MessageCollector collector, JobMetrics metrics,
                                   TelemetryValidatorConfig config) {
@@ -29,25 +25,13 @@ public class TelemetryValidatorSink extends BaseSink {
 
     public void toFailedTopic(Event event, String failedMessage) {
     	event.markFailure(failedMessage, config);
-        try {
-            toTopic(config.failedTopic(), event.mid(), event.getJson());
-        } catch (Exception ex) {
-            LOGGER.error(null, format(
-                    "EXCEPTION. PASSING EVENT MID {0} THROUGH FROM FAILED TOPIC. EXCEPTION: ",
-                    event.mid()), ex.getMessage());
-        }
+    	toTopic(config.failedTopic(), event.mid(), event.getJson());
         metrics.incFailedCounter();
     }
 
     public void toErrorTopic(Event event, String errorMessage) {
     	event.markFailure(errorMessage, config);
-    	try {
-            toTopic(config.failedTopic(), event.mid(), event.getJson());
-        } catch (Exception ex) {
-            LOGGER.error(null, format(
-                    "EXCEPTION. PASSING EVENT MID {0} THROUGH FROM FAILED TOPIC. EXCEPTION: ",
-                    event.mid()), ex.getMessage());
-        }
+    	toTopic(config.failedTopic(), event.mid(), event.getJson());
         metrics.incErrorCounter();
     }
 
