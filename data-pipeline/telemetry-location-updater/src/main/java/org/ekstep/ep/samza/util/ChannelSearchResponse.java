244c93a06e5f0331c2038670201f2cea27c7498b
Sowmya <sowmyadixit7@gmail.com>
2019-01-07 12:24:26 +0530

    Issue #SB-10113 fix: Fix channel search api response schema

--- data-pipeline/telemetry-location-updater/src/main/java/org/ekstep/ep/samza/util/ChannelSearchResponse.java
+++ data-pipeline/telemetry-location-updater/src/main/java/org/ekstep/ep/samza/util/ChannelSearchResponse.java
@@ -10,20 +10,21 @@ public class ChannelSearchResponse {
     private String ver;
     private String ts;
     private Map<String, Object> params;
-    private SearchResult result;
+    private SearchResponse result;
 
     public boolean successful() {
         return params != null && SUCCESS_RESPONSE_STATUS.equals(params.get("status"));
     }
 
     public List<String> value() {
-        if (result.count > 0 && result.org.size() > 0) {
-            return result.org.get(0).locationIds();
+        if (result.response.count > 0 && result.response.org.size() > 0) {
+            return result.response.org.get(0).locationIds();
         }
         return null;
     }
 
     private class SearchResult {
+
         private List<OrgObject> org;
         private Integer count;
 
@@ -32,6 +33,16 @@ public class ChannelSearchResponse {
             return "SearchResult{" +
                     "content=" + org +
                     ", count=" + count +
+                    '}';
+        }
+    }
+
+    private class SearchResponse {
+        private SearchResult response;
+
+        @Override
+        public String toString() {
+            return "Response{" + response +
                     '}';
         }
     }
