c9f3bb0a0cd3f091ef0d58118f0e151f0298a01b
sunil <sunils@ilimi.in>
2019-01-29 13:39:28 +0530

    Issue #SC-777 update: Additional Static information for telemetry stamping
    changes:
    - naming convention

--- data-pipeline/telemetry-location-updater/src/main/java/org/ekstep/ep/samza/domain/Event.java
+++ data-pipeline/telemetry-location-updater/src/main/java/org/ekstep/ep/samza/domain/Event.java
@@ -117,9 +117,9 @@ public class Event {
 
 	public void addLocation(Location location) {
 		Map<String, String> ldata = new HashMap<>();
-		ldata.put("countryCode", location.getCountryCode());
+		ldata.put("countrycode", location.getCountryCode());
 		ldata.put("country", location.getCountry());
-		ldata.put("stateCode", location.getStateCode());
+		ldata.put("statecode", location.getStateCode());
 		ldata.put("state", location.getState());
 		ldata.put("city", location.getCity());
 		telemetry.add(path.deviceData(), ldata);
