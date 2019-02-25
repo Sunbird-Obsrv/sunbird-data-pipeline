d1f92c66ec1668405f76ad7f689caa77f07d07a2
Sowmya <sowmyadixit7@gmail.com>
2019-02-25 18:26:35 +0530

    Issue #SB-10365 fix: Samza job to convert string timestamp field to epoch long value in content metadata.

--- data-pipeline/de-normalization/src/main/java/org/ekstep/ep/samza/domain/ContentDataUpdater.java
+++ data-pipeline/de-normalization/src/main/java/org/ekstep/ep/samza/domain/ContentDataUpdater.java
@@ -1,6 +1,14 @@
 package org.ekstep.ep.samza.domain;
 
+import org.ekstep.ep.samza.task.DeNormalizationConfig;
 import org.ekstep.ep.samza.util.ContentDataCache;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import static java.text.MessageFormat.format;
 
 public class ContentDataUpdater extends IEventUpdater {
 
@@ -10,7 +18,63 @@ public class ContentDataUpdater extends IEventUpdater {
     }
 
     public Event update(Event event) {
-        return event;
+
+        Map data;
+        String key = event.getKey("content").get(0);
+        List<Map> dataMap = new ArrayList<>();
+        try {
+            if (key != null && !key.isEmpty()) {
+                data = dataCache.getData(key);
+                if (data != null && !data.isEmpty()) {
+                    // convert string to epoch time format
+                    dataMap.add(getEpochConvertedDataMap(data));
+                    event.addMetaData(cacheType, dataMap);
+                }
+                else {
+                    event.setFlag(DeNormalizationConfig.getContentLocationJobFlag(), false);
+                }
+            }
+            return event;
+        } catch(Exception ex) {
+            LOGGER.error(null,
+                    format("EXCEPTION. EVENT: {0}, EXCEPTION:",
+                            event),
+                    ex);
+            return event;
+        }
+
+    }
+
+    public Long getTimestamp(String ts) {
+        DateTimeFormatter df = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ").withZoneUTC();
+        try {
+            return df.parseDateTime(ts).getMillis();
+        } catch(Exception ex) {
+            return 0L;
+        }
+    }
+
+    public Map getEpochConvertedDataMap(Map data) {
+
+        Object lastSubmittedOn = data.get("lastsubmittedon");
+        Object lastUpdatedOn = data.get("lastupdatedon");
+        Object lastPublishedOn = data.get("lastpublishedon");
+        if (lastSubmittedOn instanceof String) {
+            lastSubmittedOn = getTimestamp(lastSubmittedOn.toString());
+        }
+        if (lastUpdatedOn instanceof String) {
+            lastUpdatedOn = getTimestamp(lastUpdatedOn.toString());
+        }
+        if (lastPublishedOn instanceof String) {
+            lastPublishedOn = getTimestamp(lastPublishedOn.toString());
+        }
+        data.remove("lastsubmittedon");
+        data.remove("lastupdatedon");
+        data.remove("lastpublishedon");
+        data.put("lastsubmittedon", lastSubmittedOn);
+        data.put("lastupdatedon", lastUpdatedOn);
+        data.put("lastpublishedon", lastPublishedOn);
+        return data;
     }
 
 }
