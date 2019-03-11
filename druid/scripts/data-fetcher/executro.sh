b4063d75ced785fe254ffdb8a95fc3d4b85752b0
Manjunath Davanam <manjunathd@ilimi.in>
2019-03-11 18:10:52 +0530

    Issue SC-865 fix: Data fetcher script

---
+++ druid/scripts/data-fetcher/executro.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a comment!
+echo Started executing script
+#echo $script_dir
+export SPARK_HOME="/Users/admin/opt/spark-2.0.1-bin-hadoop2.7"
+export JOB_LOGS="$PWD/logs"
+export FETCHER_JAR_PATH="$PWD/target/data-fetcher-1.0.jar"
+echo "Starting the job - $1" >> "$JOB_LOGS/$today-data-fetcher-output.log"
+today=$(date "+%Y-%m-%d")
+nohup $SPARK_HOME/bin/spark-submit --master local[*] --class eventFetcher.EventsFetcher $FETCHER_JAR_PATH "$1 $2 $3 $4" >> "$JOB_LOGS/$today-data-fetcher-output.log"
+echo "Job execution completed - fetched from $1" >> "$JOB_LOGS/$today-data-fetcher-output.log"
+
+echo "Finished data pusing to kafka for this $2 to $3 date"
