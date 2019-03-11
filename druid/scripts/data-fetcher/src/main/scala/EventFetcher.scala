5a43b634c48b2a6f3e944313d758064f9d885499
Manjunath Davanam <manjunathd@ilimi.in>
2019-03-11 18:33:57 +0530

    Issue SC-865 fix: Data fetcher script

--- druid/scripts/data-fetcher/src/main/scala/EventFetcher.scala
+++ druid/scripts/data-fetcher/src/main/scala/EventFetcher.scala
@@ -6,34 +6,45 @@ import org.ekstep.analytics.framework.dispatcher.KafkaDispatcher
 import org.ekstep.analytics.framework.util.CommonUtil
 import org.apache.spark.rdd.RDD
 
-object EventsFetcher{
+object EventsFetcher {
 
   def main(args: Array[String]): Unit = {
     implicit val sc: SparkContext = CommonUtil.getSparkContext(10, "fetcher");
     val arguments = args(0).split(" ")
-    println("Arguments are:" + arguments(0),arguments(1),arguments(2),arguments(3))
+    val env = System.getenv("env")
+    var topic = env + ".events.telemetry";
+    var path = ""
+    if (arguments(3) == "summary") {
+      path = "derived/summary/"
+      topic = env + ".events.summary"
+    } else {
+      path = "raw/"
+      topic = env + ".events.telemetry"
+    }
+
+    println("Arguments are:" + arguments(0), arguments(1), arguments(2), arguments(3))
     val data: RDD[String] =
-      if(arguments(0) == "s3"){
+      if (arguments(0) == "s3") {
         println("Fetching from S3")
         sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", System.getenv("aws_storage_key"));
-        sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey",System.getenv("aws_storage_secret"));
+        sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", System.getenv("aws_storage_secret"));
         println(System.getenv("aws_storage_key"))
         println(System.getenv("aws_storage_secret"))
-        val s3folder = Option(Array(Query(Option("ekstep-prod-data-store"), Option(arguments(3)), Option(arguments(1)), Option(arguments(2)))))
+        val s3folder = Option(Array(Query(Option("ekstep-prod-data-store"), Option(path), Option(arguments(1)), Option(arguments(2)))))
         val data = DataFetcher.fetchBatchData[String](Fetcher("s3", None, s3folder))
-        println("S3 Data counts form: " + arguments(1) + "to" + arguments(2) +":" + data.count)
+        println("S3 Data counts form: " + arguments(1) + "to" + arguments(2) + ":" + data.count)
         data
-      }else{
+      } else {
         println("azure")
         sc.hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
-        sc.hadoopConfiguration.set("fs.azure.account.key."+System.getenv("azure_storage_key")+".blob.core.windows.net", System.getenv("azure_storage_secret"))
-        val azureFolder = Option(Array(Query(Option("telemetry-data-store"), Option(arguments(3)), Option(arguments(1)), Option(arguments(2)))))
+        sc.hadoopConfiguration.set("fs.azure.account.key." + System.getenv("azure_storage_key") + ".blob.core.windows.net", System.getenv("azure_storage_secret"))
+        val azureFolder = Option(Array(Query(Option("telemetry-data-store"), Option(path), Option(arguments(1)), Option(arguments(2)))))
         val data = DataFetcher.fetchBatchData[String](Fetcher("azure", None, azureFolder))
-        println("Azure Data counts from: " + arguments(1) + "to" + arguments(2) +":" + data.count)
+        println("Azure Data counts from: " + arguments(1) + "to" + arguments(2) + ":" + data.count)
         data
       }
-    println("Count is", data.count)
-    val config = Map("brokerList" -> "localhost:9092", "topic" -> "telemetry_events")
+
+    val config = Map("brokerList" -> "localhost:9092", "topic" -> topic)
     KafkaDispatcher.dispatch(config, data)
   }
 
