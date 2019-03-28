02023b096b1c8b608337299a924ef6a0e4bceeda
Manjunath Davanam <manjunathd@ilimi.in>
2019-03-28 18:30:19 +0530

    Issue SC-865 fix: Shell script code to execute the jar file

--- druid/telemetry-fetcher/src/run.sh
+++ druid/telemetry-fetcher/src/run.sh
@@ -1,28 +1,61 @@
-#!/usr/bin/env bash
-#Example: ./run.sh -s 2019-03-10 -e 2019-03-10 -v local -c azure -t summary -p derived/wfs/
+#!/bin/bash
+#Example : ./run.sh --startDate 2019-03-10 --endDate 2019-03-10 --env local --service azure --eventType summary --prefix derived/wfs/
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-while getopts ":s:e:c:t:b:v:p:" arguments; do
-  case $arguments in
-    s) startDate="$OPTARG"
+case $key in
+    -s|--startDate)
+    STARTDATE="$2"
+    shift # past argument
+    shift # past value
     ;;
-    e) endDate="$OPTARG"
+    -e|--endDate)
+    ENDDATE="$2"
+    shift # past argument
+    shift # past value
     ;;
-    c) service="$OPTARG"
+    -c|--service)
+    SERVICE="$2"
+    shift # past argument
+    shift # past value
     ;;
-    t) eventType="$OPTARG"
+    -t|--eventType)
+    EVENTTYPE="$2"
+    shift # past argument
+    shift # past value
     ;;
-    b) bucket="$OPTARG"
+    -b|--bucket)
+    BUCKET="$2"
+    shift # past argument
+    shift # past value
     ;;
-    v) env="$OPTARG"
+    -v|--env)
+    ENV="$2"
+    shift # past argument
+    shift # past value
     ;;
-    p) prefix="$OPTARG"
+    -p|--prefix)
+    PREFIX="$2"
+    shift # past argument
+    shift # past value
     ;;
-    \?) echo "Invalid arguments -$OPTARG" >&2
+    --default)
+    DEFAULT=YES
+    shift # past argument
     ;;
-  esac
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
 done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
 today=$(date "+%Y-%m-%d-%h-%m")
-echo "Started executing the script to fetch the data from $startDate to $endDate date from the $service service with this $prefix location prefix "
+export TELEMETRY_FETCHER_JAR_PATH="/Users/admin/Documents/sunbird-forked-repo/sunbird-data-pipeline/druid/telemetry-fetcher/target/telemetry-fetcher-1.0.jar"
+echo "Started executing the script to fetch the data from $STARTDATE to $ENDDATE date from the $SERVICE service with this $PREFIX location prefix "
 export JOB_LOGS="$PWD/logs"
-nohup $SPARK_HOME/bin/spark-submit --master local[*] --class org.sunbird.EventsFetcher $TELEMETRY_FETCHER_JAR_PATH --startDate $startDate --endDate $endDate --env $env --service $service --eventType $eventType --prefix $prefix >> "$JOB_LOGS/$today-events-fetcher-output.log"
+nohup $SPARK_HOME/bin/spark-submit --master local[*] --class org.sunbird.EventsFetcher $TELEMETRY_FETCHER_JAR_PATH --startDate $STARTDATE --endDate $ENDDATE --env $ENV --service $SERVICE --eventType $EVENTTYPE --prefix $PREFIX --bucket $BUCKET >> "$JOB_LOGS/$today-events-fetcher-output.log"
 echo "Script execution completed please check the status in this $JOB_LOGS/$today-events-fetcher-output.log file"
