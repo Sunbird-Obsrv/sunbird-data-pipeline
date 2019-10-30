ffabb5691c754d106ca38daf1c217117eeb5b902
RevathiKotla <revathi21kotla@gmail.com>
2019-10-30 17:36:51 +0530

    Issue #SB-14322 : merging change to release-2.5.0

--- data-pipeline/derived-de-duplication/src/main/java/org/ekstep/ep/samza/domain/Event.java
+++ data-pipeline/derived-de-duplication/src/main/java/org/ekstep/ep/samza/domain/Event.java
@@ -79,35 +79,35 @@ public class Event {
 
     public void markSkipped() {
         telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
-        telemetry.add("flags.dd_processed", false);
-        telemetry.add("flags.dd_checksum_present", false);
+        telemetry.add("flags.derived_dd_processed", false);
+        telemetry.add("flags.derived_dd_checksum_present", false);
     }
 
     public void markDuplicate() {
         telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
-        telemetry.add("flags.dd_processed", false);
-        telemetry.add("flags.dd_duplicate_event", true);
+        telemetry.add("flags.derived_dd_processed", false);
+        telemetry.add("flags.derived_dd_duplicate_event", true);
     }
 
     public void markSuccess() {
         telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
-        telemetry.add("flags.dd_processed", true);
+        telemetry.add("flags.derived_dd_processed", true);
         telemetry.add("type", "events");
     }
 
     public void markRedisFailure() {
         telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
-        telemetry.add("flags.dd_processed", false);
-        telemetry.add("flags.dd_redis_failure", true);
+        telemetry.add("flags.derived_dd_processed", false);
+        telemetry.add("flags.derived_dd_redis_failure", true);
         telemetry.add("type", "events");
     }
 
     public void markFailure(String error, DeDuplicationConfig config) {
         telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
-        telemetry.add("flags.dd_processed", false);
+        telemetry.add("flags.derived_dd_processed", false);
 
         telemetry.addFieldIfAbsent("metadata", new HashMap<String, Object>());
-        telemetry.add("metadata.dd_error", error);
+        telemetry.add("metadata.derived_dd_error", error);
         telemetry.add("metadata.src", config.jobName());
     }
 
