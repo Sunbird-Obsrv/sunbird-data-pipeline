4d5f9ec7461c2a3a39d0521d1b640c71ceaace92
Manjunath Davanam <manjunathd@ilimi.in>
2019-11-21 17:26:13 +0530

    Issue #SC-1482 fix: Running api as docker

---
+++ druid/druid-api/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Build script
+# set -o errexit
+e () {
+    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
+}
+m=$(./src/metadata.sh)
+
+org=$(e "${m}" "org")
+name=$(e "${m}" "name")
+version=$(e "${m}" "version")
+
+
+docker build -f ./Dockerfile.Build -t ${org}/${name}:${version}-build . 
+docker run --name=${name}-${version}-build ${org}/${name}:${version}-build 
+containerid=`docker ps -aqf "name=${name}-${version}-build"`
+docker cp $containerid:/opt/telemetry-service.zip telemetry-service.zip
+docker rm $containerid
+docker build -f ./Dockerfile -t ${org}/${name}:${version}-bronze .
