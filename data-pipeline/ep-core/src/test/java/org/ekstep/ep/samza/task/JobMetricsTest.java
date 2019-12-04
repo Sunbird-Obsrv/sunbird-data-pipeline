96aaf04c3a0cc1cca7b7efb508ffe48bf628a7c6
Manjunath Davanam <manjunathd@ilimi.in>
2019-12-04 15:16:09 +0530

    Issue SC-1482 fix: ep-core test case improvement

--- data-pipeline/ep-core/src/test/java/org/ekstep/ep/samza/task/JobMetricsTest.java
+++ data-pipeline/ep-core/src/test/java/org/ekstep/ep/samza/task/JobMetricsTest.java
@@ -21,51 +21,52 @@ import static org.mockito.Mockito.*;
 
 public class JobMetricsTest {
 
-	private TaskContext contextMock;
-	private JobMetrics jobMetricsMock;
+    private TaskContext contextMock;
+    private JobMetrics jobMetricsMock;
 
-	@Before
-	public void setUp() {
-		contextMock = mock(TaskContext.class);
-		MetricsRegistry metricsRegistry = mock(MetricsRegistry.class);
-		Counter counter = mock(Counter.class);
-		stub(metricsRegistry.newCounter(anyString(), anyString())).toReturn(counter);
-		stub(contextMock.getMetricsRegistry()).toReturn(metricsRegistry);
-	}
+    @Before
+    public void setUp() {
+        contextMock = mock(TaskContext.class);
+        MetricsRegistry metricsRegistry = mock(MetricsRegistry.class);
+        Counter counter = mock(Counter.class);
+        stub(metricsRegistry.newCounter(anyString(), anyString())).toReturn(counter);
+        stub(contextMock.getMetricsRegistry()).toReturn(metricsRegistry);
+    }
 
-	@Test
-	public void shouldReturnConsumerLag() {
+    @Test
+    public void shouldReturnConsumerLag() {
 
-		jobMetricsMock = new JobMetrics(contextMock, null);
+        jobMetricsMock = new JobMetrics(contextMock, "test-job");
+        jobMetricsMock.clear();
+        Set<SystemStreamPartition> systemStreamPartitions = new HashSet<>();
+        SystemStreamPartition systemStreamPartition =
+                new SystemStreamPartition("kafka", "inputtopic1", new Partition(0));
+        systemStreamPartitions.add(systemStreamPartition);
 
-		Set<SystemStreamPartition> systemStreamPartitions = new HashSet<>();
-		SystemStreamPartition systemStreamPartition =
-				new SystemStreamPartition("kafka", "inputtopic1", new Partition(0));
-		systemStreamPartitions.add(systemStreamPartition);
+        Map<String, ConcurrentHashMap<String, Metric>> concurrentHashMap =
+                MetricsFixture.getMetricMap(MetricsFixture.METRIC_EVENT_STREAM1);
+        when(contextMock.getSystemStreamPartitions()).thenReturn(systemStreamPartitions);
+        long consumer_lag = jobMetricsMock.consumerLag(concurrentHashMap);
+        Assert.assertEquals(800, consumer_lag);
 
-		Map<String, ConcurrentHashMap<String, Metric>> concurrentHashMap =
-				MetricsFixture.getMetricMap(MetricsFixture.METRIC_EVENT_STREAM1);
-		when(contextMock.getSystemStreamPartitions()).thenReturn(systemStreamPartitions);
-		long consumer_lag = jobMetricsMock.consumerLag(concurrentHashMap);
-		Assert.assertEquals(800, consumer_lag);
+    }
 
-	}
+    @Test
+    public void shouldReturnZeroConsumerLagWhenAllMessagesAreProcessed() {
 
-	@Test
-	public void shouldReturnZeroConsumerLagWhenAllMessagesAreProcessed() {
+        jobMetricsMock = new JobMetrics(contextMock, null);
 
-		jobMetricsMock = new JobMetrics(contextMock, null);
+        Set<SystemStreamPartition> systemStreamPartitions = new HashSet<>();
+        SystemStreamPartition systemStreamPartition =
+                new SystemStreamPartition("kafka", "inputtopic1", new Partition(0));
+        systemStreamPartitions.add(systemStreamPartition);
 
-		Set<SystemStreamPartition> systemStreamPartitions = new HashSet<>();
-		SystemStreamPartition systemStreamPartition =
-				new SystemStreamPartition("kafka", "inputtopic1", new Partition(0));
-		systemStreamPartitions.add(systemStreamPartition);
+        Map<String, ConcurrentHashMap<String, Metric>> concurrentHashMap =
+                MetricsFixture.getMetricMap(MetricsFixture.METRIC_EVENT_STREAM2);
+        when(contextMock.getSystemStreamPartitions()).thenReturn(systemStreamPartitions);
+        long consumer_lag = jobMetricsMock.consumerLag(concurrentHashMap);
+        Assert.assertEquals(0, consumer_lag);
+    }
 
-		Map<String, ConcurrentHashMap<String, Metric>> concurrentHashMap =
-				MetricsFixture.getMetricMap(MetricsFixture.METRIC_EVENT_STREAM2);
-		when(contextMock.getSystemStreamPartitions()).thenReturn(systemStreamPartitions);
-		long consumer_lag = jobMetricsMock.consumerLag(concurrentHashMap);
-		Assert.assertEquals(0, consumer_lag);
 
-	}
 }
