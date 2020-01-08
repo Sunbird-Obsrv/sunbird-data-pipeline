e3f3f0438639ad87d8b88b0349122dde5e94ad9c
RevathiKotla <revathi21kotla@gmail.com>
2020-01-08 16:13:46 +0530

    Issue #SC-000 : Fix date issue for grand total in report

--- data-pipeline/assessment-aggregator/src/main/java/org/ekstep/ep/samza/domain/Aggregate.java
+++ data-pipeline/assessment-aggregator/src/main/java/org/ekstep/ep/samza/domain/Aggregate.java
@@ -11,6 +11,7 @@ public class Aggregate {
     private double totalScore;
     private double totalMaxScore;
     private List<UDTValue> questionsList;
+    private DecimalFormat df = new DecimalFormat("###.0#");
 
     public Aggregate(double totalScore, double totalMaxScore, List<UDTValue> questionsList) {
 
@@ -29,7 +30,7 @@ public class Aggregate {
     }
 
     public String getGrandTotal() {
-        return String.format("%s/%s", totalScore, totalMaxScore);
+        return String.format("%s/%s", df.format(totalScore), df.format(totalMaxScore));
     }
 
     public List<UDTValue> getQuestionsList() {
