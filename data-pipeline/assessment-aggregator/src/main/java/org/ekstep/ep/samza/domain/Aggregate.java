4d59f7d7204208582635d8ba46a11fc1e509c64a
RevathiKotla <revathi21kotla@gmail.com>
2020-01-17 15:13:24 +0530

    Issue #SC-000- Fix decimal issue in assessment

--- data-pipeline/assessment-aggregator/src/main/java/org/ekstep/ep/samza/domain/Aggregate.java
+++ data-pipeline/assessment-aggregator/src/main/java/org/ekstep/ep/samza/domain/Aggregate.java
@@ -11,7 +11,7 @@ public class Aggregate {
     private double totalScore;
     private double totalMaxScore;
     private List<UDTValue> questionsList;
-    private DecimalFormat df = new DecimalFormat("###.0#");
+    private DecimalFormat df = new DecimalFormat("0.0#");
 
     public Aggregate(double totalScore, double totalMaxScore, List<UDTValue> questionsList) {
 
