c46b8d9f3d7b0b45d675a3761af70145df6e803c
Manjunath Davanam <manjunathd@ilimi.in>
2020-03-30 13:42:21 +0530

    Issue #SC-1726 fix: Added logs for dedup to extraction job

--- data-pipeline-flink/telemetry-extractor/src/main/scala/org/sunbird/dp/functions/DeduplicationFunction.scala
+++ data-pipeline-flink/telemetry-extractor/src/main/scala/org/sunbird/dp/functions/DeduplicationFunction.scala
@@ -41,35 +41,35 @@ class DeduplicationFunction(config: ExtractionConfig, @transient var dedupEngine
   }
 
   override def processElement(
-                               event: util.Map[String, AnyRef],
+                               batchEvents: util.Map[String, AnyRef],
                                context: ProcessFunction[util.Map[String, AnyRef], util.Map[String, AnyRef]]#Context,
                                out: Collector[util.Map[String, AnyRef]]
                              ): Unit = {
 
     if (config.isDuplicationCheckRequired) {
-      val msgId = getMsgIdentifier(event)
-      if (null != msgId) {
-        if (!dedupEngine.isUniqueEvent(msgId)) {
-          logger.info(s"Duplicate Event message id: ${msgId}")
-          context.output(duplicateEventOutput, event)
-        } else {
-          logger.info(s"Adding mid: ${msgId} to Redis")
-          dedupEngine.storeChecksum(msgId)
-          context.output(uniqueEventOutput, event)
-        }
+      val msgId = getMsgIdentifier(batchEvents)
+      if (null != msgId && !dedupEngine.isUniqueEvent(msgId)) {
+        logger.info(s"Duplicate Event message id is found: ${msgId}")
+        context.output(duplicateEventOutput, batchEvents)
       } else {
-        context.output(uniqueEventOutput, event)
+        if (msgId != null) {
+          logger.info(s"Adding msgId: ${msgId} to Redis")
+          dedupEngine.storeChecksum(msgId)
+        }
+        logger.info(s"Pushing batch event to extraction process MessageId: ${msgId} and MID: ${batchEvents.get("mid")}")
+        context.output(uniqueEventOutput, batchEvents)
       }
     } else {
-      context.output(uniqueEventOutput, event)
+      logger.info(s"de-dup is disabled hence Pushing batch event to extraction process")
+      context.output(uniqueEventOutput, batchEvents)
     }
   }
 
-  def getMsgIdentifier(event: util.Map[String, AnyRef]): String = {
+  def getMsgIdentifier(batchEvents: util.Map[String, AnyRef]): String = {
     val gson = new Gson();
-    val paramsObj = event.get("params")
+    val paramsObj = batchEvents.get("params")
     if (null != paramsObj) {
-      val messageId = gson.fromJson(gson.toJson(paramsObj), (new util.LinkedHashMap[String, AnyRef]()).getClass).get("msgid")
+      val messageId = gson.fromJson(gson.toJson(paramsObj), (new util.HashMap[String, AnyRef]()).getClass).get("msgid")
       if (null != messageId)
         messageId.toString
       else null
