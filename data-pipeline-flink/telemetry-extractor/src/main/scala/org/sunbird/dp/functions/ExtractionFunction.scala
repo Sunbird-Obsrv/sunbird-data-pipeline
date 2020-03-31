ea1a35a3de5d9b2e1dacdec43b6dae8f0f7db5f1
Anand <anandp@ilimi.in>
2020-03-31 13:22:38 +0530

    Issue #SC-1726 chore: Refactor Extractor job

--- data-pipeline-flink/telemetry-extractor/src/main/scala/org/sunbird/dp/functions/ExtractionFunction.scala
+++ data-pipeline-flink/telemetry-extractor/src/main/scala/org/sunbird/dp/functions/ExtractionFunction.scala
@@ -1,18 +1,23 @@
 package org.sunbird.dp.functions
 
+import java.lang.reflect.Type
 import java.util
 
 import com.google.gson.Gson
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.functions.ProcessFunction
-import org.apache.flink.streaming.api.scala.OutputTag
 import org.apache.flink.util.Collector
 import org.joda.time.format.DateTimeFormat
 import org.sunbird.dp.task.ExtractionConfig
 import org.sunbird.dp.domain._
 import java.util.UUID
 
-class ExtractionFunction(config: ExtractionConfig)(implicit val eventTypeInfo: TypeInformation[util.Map[String, AnyRef]]) extends ProcessFunction[util.Map[String, AnyRef], util.Map[String, AnyRef]] {
+import com.google.gson.reflect.TypeToken
+
+class ExtractionFunction(config: ExtractionConfig)(implicit val stringTypeInfo: TypeInformation[String])
+  extends ProcessFunction[util.Map[String, AnyRef], String] {
+
+  val mapType: Type = new TypeToken[util.Map[String, AnyRef]](){}.getType
 
   /**
    * Method to process the events extraction from the batch
@@ -21,24 +26,31 @@ class ExtractionFunction(config: ExtractionConfig)(implicit val eventTypeInfo: T
    * @param context
    * @param collector
    */
-  override def processElement(batchEvent: util.Map[String, AnyRef], context: ProcessFunction[util.Map[String, AnyRef], util.Map[String, AnyRef]]#Context, collector: Collector[util.Map[String, AnyRef]]): Unit = {
-    val gson = new Gson();
+  override def processElement(batchEvent: util.Map[String, AnyRef],
+                              context: ProcessFunction[util.Map[String, AnyRef], String]#Context,
+                              collector: Collector[String]): Unit = {
+
+    val gson = new Gson()
     val eventsList = getEventsList(batchEvent)
-    eventsList.foreach(event => {
-      val eventJson = gson.toJson(event)
-      val eventSize = eventJson.getBytes("UTF-8").length;
-      val eventData = updateEvent(batchEvent, gson.fromJson(eventJson, (new util.HashMap[String, AnyRef]()).getClass))
+    eventsList.forEach(event => {
+
+      val syncts = batchEvent.get("syncts").asInstanceOf[Number].longValue()
+      val eventData = updateEvent(event, syncts)
+      val eventJson = gson.toJson(eventData)
+      val eventSize = eventJson.getBytes("UTF-8").length
+
       if (eventSize > config.eventMaxSize) {
-        context.output(config.failedEventsOutputTag, eventData)
+        context.output(config.failedEventsOutputTag, eventJson)
       } else {
-        context.output(config.rawEventsOutputTag, eventData)
+        context.output(config.rawEventsOutputTag, eventJson)
       }
     })
 
     /**
      * Generating Audit events to compute the number of events in the batch.
      */
-    context.output(config.logEventsOutputTag, gson.fromJson(gson.toJson(generateAuditEvents(eventsList.length, batchEvent)), (new util.HashMap[String, AnyRef]()).getClass))
+    context.output(config.logEventsOutputTag,
+      gson.fromJson(gson.toJson(generateAuditEvents(eventsList.size())), mapType))
   }
 
   /**
@@ -47,30 +59,27 @@ class ExtractionFunction(config: ExtractionConfig)(implicit val eventTypeInfo: T
    * @param batchEvent - Batch of telemetry event.
    * @return Array[AnyRef] - List of telemetry events.
    */
-  def getEventsList(batchEvent: util.Map[String, AnyRef]): Array[AnyRef] = {
-    val gson = new Gson();
-    gson.fromJson(gson.toJson(batchEvent.get("events")), (new util.ArrayList[String]()).getClass).toArray
+  def getEventsList(batchEvent: util.Map[String, AnyRef]): util.ArrayList[util.Map[String, AnyRef]] = {
+    batchEvent.get("events").asInstanceOf[util.ArrayList[util.Map[String, AnyRef]]]
   }
 
   /**
    * Method to update the "SyncTS", "@TimeStamp" fileds of batch events into Events Object
-   *
-   * @param batchEvent - Batch of Telemetry Events.
-   * @param event      - Telemetry Event
+   * @param event - Extracted Raw Telemetry Event
+   * @param syncts - sync timestamp epoch to be updated in the events
    * @return - util.Map[String, AnyRef] Updated Telemetry Event
    */
-  def updateEvent(batchEvent: util.Map[String, AnyRef], event: util.Map[String, AnyRef]): util.Map[String, AnyRef] = {
-    val syncTs: Long = batchEvent.get("syncts").asInstanceOf[Number].longValue()
-    val timeStamp: String = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC.print(syncTs)
-    event.put("syncts", syncTs.asInstanceOf[AnyRef])
-    event.put("@timestamp", timeStamp.asInstanceOf[AnyRef])
+  def updateEvent(event: util.Map[String, AnyRef], syncts: Long): util.Map[String, AnyRef] = {
+    val timeStampString: String = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC.print(syncts)
+    event.put("syncts", syncts.asInstanceOf[AnyRef])
+    event.put("@timestamp", timeStampString.asInstanceOf[AnyRef])
     event
   }
 
   /**
    * Method to Generate the LOG Event to Determine the Number of events has extracted.
    */
-  def generateAuditEvents(totalEvents: Int, batchEvents: util.Map[String, AnyRef]): LogEvent = {
+  def generateAuditEvents(totalEvents: Int): LogEvent = {
     LogEvent(
       actor = Actor("sunbird.telemetry", "telemetry-sync"),
       eid = "LOG",
@@ -80,7 +89,7 @@ class ExtractionFunction(config: ExtractionConfig)(implicit val eventTypeInfo: T
       context = org.sunbird.dp.domain.Context(channel = "in.sunbird", env = "data-pipeline",
         sid = UUID.randomUUID().toString,
         did = UUID.randomUUID().toString,
-        pdata = Pdata("3.0", "telemetry-extractor", "data-pipeline"),
+        pdata = Pdata(ver = "3.0", pid = "telemetry-extractor"),
         cdata = null),
       mid = UUID.randomUUID().toString,
       `object` = Object(UUID.randomUUID().toString, "3.0", "telemetry-events", None),
