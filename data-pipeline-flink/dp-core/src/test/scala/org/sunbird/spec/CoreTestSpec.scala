1b9914d486abf8f4e458aceed38a9dd1e2f9cdd1
Manjunath Davanam <manjunathd@ilimi.in>
2020-04-16 00:37:56 +0530

    Issue SC-1727 feat: Testcase fixes

--- data-pipeline-flink/dp-core/src/test/scala/org/sunbird/spec/CoreTestSpec.scala
+++ data-pipeline-flink/dp-core/src/test/scala/org/sunbird/spec/CoreTestSpec.scala
@@ -27,13 +27,13 @@ class CoreTestSpec extends BaseSpec with Matchers with MockitoSugar {
       |"version":"","rollup":{}}}""".stripMargin
 
 
-  "Telemetry" should "Able to parse the object" in {
-    val gson = new Gson()
-    val event1 = gson.fromJson(EVENT_WITH_MID, new util.LinkedHashMap[String, Any]().getClass)
-   val event = new Event(event1)
-
-    println("event.did()" + event.did())
-//   //println("telemetry.read(\"eid\").value" + telemetry.read("eid").value)
+//  "Telemetry" should "Able to parse the object" in {
+//    val gson = new Gson()
+//    val event1 = gson.fromJson(EVENT_WITH_MID, new util.LinkedHashMap[String, Any]().getClass)
+//    val event = new Event(event1)
+//
+//    println("event.did()" + event.did())
+//    //println("telemetry.read(\"eid\").value" + telemetry.read("eid").value)
 //    println("event.mid()" + event.eid())
 //    println("event.mid()" + event.edataType())
 //    println("event.mid()" + event.edataType())
@@ -51,127 +51,127 @@ class CoreTestSpec extends BaseSpec with Matchers with MockitoSugar {
 //    println("acto" + event.channel())
 //    val la = event.markSuccess("test")
 //    println("la" + gson.toJson(event))
+//
+//  }
+
+
+  "RedisConnection" should "Able to connect to redis" in {
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+    val status = redisConnection.getConnection(2)
+    status.isConnected should be(true)
+  }
+
+  it should "Able to reset the redis connection" in {
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+    val status = redisConnection.getConnection(2)
+    status.isConnected should be(true)
+    val resetStatus = redisConnection.resetConnection(2)
+    resetStatus.isConnected should be(true)
+  }
+
+  it should "Able to close the redis connection" in intercept[JedisConnectionException] {
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+    val status = redisConnection.getConnection(2)
+    status.isConnected should be(true)
+    redisConnection.closePool
+    val reConnectionStatus = redisConnection.getConnection(2)
+    reConnectionStatus.isConnected should be(false)
+  }
+
+  "De-DupEngine" should "Able to detect the key is unique or not" in {
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+
+    val dedupEngine = new DedupEngine(redisConnection, 2, 200)
+    dedupEngine.isUniqueEvent("key-1") should be(true)
+    dedupEngine.storeChecksum("key-1")
+    dedupEngine.isUniqueEvent("key-1") should be(false)
 
   }
 
+  it should "Able to reConnect when the jedis exception thrown" in intercept[JedisException] {
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+    redisConnection.closePool()
+    val dedupEngine = new DedupEngine(redisConnection, 0, 4309535)
+    dedupEngine.isUniqueEvent("event-id-3") should be(true)
+  }
+  "DataCache" should "hgetAllWithRetry method should bble to get the map data" in {
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+    val map = new util.HashMap[String, String]()
+    map.put("country_code", "IN")
+    map.put("country", "INDIA")
+    redisConnection.getConnection(2).hmset("5d8947598347589fsdlh43y8", map)
+    redisConnection.getConnection(2).set("56934dufhksjd8947hdskj", "{\"actor\":{\"type\":\"User\",\"id\":\"4c4530df-0d4f-42a5-bd91-0366716c8c24\"}}")
 
-//  "RedisConnection" should "Able to connect to redis" in {
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//    val status = redisConnection.getConnection(2)
-//    status.isConnected should be(true)
-//  }
-//
-//  it should "Able to reset the redis connection" in {
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//    val status = redisConnection.getConnection(2)
-//    status.isConnected should be(true)
-//    val resetStatus = redisConnection.resetConnection(2)
-//    resetStatus.isConnected should be(true)
-//  }
-//
-//  it should "Able to close the redis connection" in intercept[JedisConnectionException] {
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//    val status = redisConnection.getConnection(2)
-//    status.isConnected should be(true)
-//    redisConnection.closePool
-//    val reConnectionStatus = redisConnection.getConnection(2)
-//    reConnectionStatus.isConnected should be(false)
-//  }
-//
-//  "De-DupEngine" should "Able to detect the key is unique or not" in {
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//
-//    val dedupEngine = new DedupEngine(redisConnection, 2, 200)
-//    dedupEngine.isUniqueEvent("key-1") should be(true)
-//    dedupEngine.storeChecksum("key-1")
-//    dedupEngine.isUniqueEvent("key-1") should be(false)
-//
-//  }
-//
-//  it should "Able to reConnect when the jedis exception thrown" in intercept[JedisException] {
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//    redisConnection.closePool()
-//    val dedupEngine = new DedupEngine(redisConnection, 0, 4309535)
-//    dedupEngine.isUniqueEvent("event-id-3") should be(true)
-//  }
-//  "DataCache" should "hgetAllWithRetry method should bble to get the map data" in {
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//    val map = new util.HashMap[String, String]()
-//    map.put("country_code", "IN")
-//    map.put("country", "INDIA")
-//    redisConnection.getConnection(2).hmset("5d8947598347589fsdlh43y8", map)
-//    redisConnection.getConnection(2).set("56934dufhksjd8947hdskj", "{\"actor\":{\"type\":\"User\",\"id\":\"4c4530df-0d4f-42a5-bd91-0366716c8c24\"}}")
-//
-//    val deviceFields = List("country_code", "country", "state_code", "st" +
-//      "ate", "city", "district_custom", "state_code_custom",
-//      "state_custom", "user_declared_state", "user_declared_district", "devicespec", "firstaccess")
-//    val dataCache = new DataCache(bsConfig, new RedisConnect(bsConfig), 2, deviceFields)
-//    dataCache.init()
-//    val hGetResponse = dataCache.hgetAllWithRetry("5d8947598347589fsdlh43y8")
-//    val hGetResponse2 = dataCache.getMultipleWithRetry(List("56934dufhksjd8947hdskj"))
-//    hGetResponse2.size should be(1)
-//    hGetResponse.size should be(2)
-//    hGetResponse.get("country_code").get should be("IN")
-//    hGetResponse.get("country").get should be("INDIA")
-//    dataCache.close()
-//  }
-//
-//  "BaseDedup" should "Should able to deDup the event" in {
-//    val deDup = new BaseDeduplication {}
-//    val metrics = deDup.deduplicationMetrics
-//    metrics.size should be(2)
-//    val event = new util.HashMap[String, AnyRef]()
-//    event.put("country_code", "IN")
-//    event.put("country", "INDIA")
-//    deDup.updateFlag[util.Map[String, AnyRef]](event, "test-failed", true)
-//    val config = ConfigFactory.load("test.conf");
-//    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
-//    val redisConnection = new RedisConnect(bsConfig)
-//    val dedupEngine = new DedupEngine(redisConnection, 2, 200)
-//    dedupEngine.storeChecksum("key-1")
-//    implicit val stringTypeInfo: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
-//    val dedupTag: OutputTag[String] = OutputTag[String]("test-de-dup-tag")
-//    val uniqueTag: OutputTag[String] = OutputTag[String]("test-unique-tag")
-//    // deDup.deDup[String]("key-1","test",null, dedupTag, uniqueTag, "test")(dedupEngine, JobMetrics.apply(List("success-count")))
-//  }
-//  "StringSerialization" should "Able to serialize the data" in {
-//    val topic: String = "topic-test"
-//    val partition: Int = 0
-//    val offset: Long = 1
-//    val group: String = "group-test"
-//    val key: Array[Byte] = null
-//    val value: Array[Byte] = Array[Byte](1)
-//    val stringDeSerialization = new StringDeserializationSchema()
-//    val stringSerialization = new StringSerializationSchema(topic)
-//    val eventSerialization = new EventSerializationSchema[Events](topic)
-//    val eventDeSerialization = new EventDeserializationSchema[Events]
-//    val mapSerialization: MapSerializationSchema = new MapSerializationSchema(topic)
-//    val mapDeSerialization = new MapDeserializationSchema()
-//    import org.apache.kafka.clients.consumer.ConsumerRecord
-//    val cRecord: ConsumerRecord[Array[Byte], Array[Byte]] = new ConsumerRecord[Array[Byte], Array[Byte]](topic, partition, offset, key, value)
-//    stringDeSerialization.deserialize(cRecord)
-//    val event = new Event(new Gson().fromJson(EVENT_WITH_MID, new util.LinkedHashMap[String, AnyRef]().getClass))
-//    eventSerialization.serialize(event, System.currentTimeMillis())
-//    eventDeSerialization.getProducedType
-//    stringSerialization.serialize("test", System.currentTimeMillis())
-//    stringDeSerialization.isEndOfStream("") should be(false)
-//    val map = new util.HashMap[String, AnyRef]()
-//    map.put("country_code", "IN")
-//    map.put("country", "INDIA")
-//    mapSerialization.serialize(map, System.currentTimeMillis())
-//  }
+    val deviceFields = List("country_code", "country", "state_code", "st" +
+      "ate", "city", "district_custom", "state_code_custom",
+      "state_custom", "user_declared_state", "user_declared_district", "devicespec", "firstaccess")
+    val dataCache = new DataCache(bsConfig, new RedisConnect(bsConfig), 2, deviceFields)
+    dataCache.init()
+    val hGetResponse = dataCache.hgetAllWithRetry("5d8947598347589fsdlh43y8")
+    val hGetResponse2 = dataCache.getMultipleWithRetry(List("56934dufhksjd8947hdskj"))
+    hGetResponse2.size should be(1)
+    hGetResponse.size should be(2)
+    hGetResponse.get("country_code").get should be("IN")
+    hGetResponse.get("country").get should be("INDIA")
+    dataCache.close()
+  }
+
+  "BaseDedup" should "Should able to deDup the event" in {
+    val deDup = new BaseDeduplication {}
+    val metrics = deDup.deduplicationMetrics
+    metrics.size should be(2)
+    val event = new util.HashMap[String, AnyRef]()
+    event.put("country_code", "IN")
+    event.put("country", "INDIA")
+    deDup.updateFlag[util.Map[String, AnyRef]](event, "test-failed", true)
+    val config = ConfigFactory.load("test.conf");
+    val bsConfig: BaseJobConfig = new BaseJobConfig(config, "base-job");
+    val redisConnection = new RedisConnect(bsConfig)
+    val dedupEngine = new DedupEngine(redisConnection, 2, 200)
+    dedupEngine.storeChecksum("key-1")
+    implicit val stringTypeInfo: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
+    val dedupTag: OutputTag[String] = OutputTag[String]("test-de-dup-tag")
+    val uniqueTag: OutputTag[String] = OutputTag[String]("test-unique-tag")
+    // deDup.deDup[String]("key-1","test",null, dedupTag, uniqueTag, "test")(dedupEngine, JobMetrics.apply(List("success-count")))
+  }
+  "StringSerialization" should "Able to serialize the data" in {
+    val topic: String = "topic-test"
+    val partition: Int = 0
+    val offset: Long = 1
+    val group: String = "group-test"
+    val key: Array[Byte] = null
+    val value: Array[Byte] = Array[Byte](1)
+    val stringDeSerialization = new StringDeserializationSchema()
+    val stringSerialization = new StringSerializationSchema(topic)
+    val eventSerialization = new EventSerializationSchema[Events](topic)
+    val eventDeSerialization = new EventDeserializationSchema[Events]
+    val mapSerialization: MapSerializationSchema = new MapSerializationSchema(topic)
+    val mapDeSerialization = new MapDeserializationSchema()
+    import org.apache.kafka.clients.consumer.ConsumerRecord
+    val cRecord: ConsumerRecord[Array[Byte], Array[Byte]] = new ConsumerRecord[Array[Byte], Array[Byte]](topic, partition, offset, key, value)
+    stringDeSerialization.deserialize(cRecord)
+    val event = new Event(new Gson().fromJson(EVENT_WITH_MID, new util.LinkedHashMap[String, AnyRef]().getClass))
+    eventSerialization.serialize(event, System.currentTimeMillis())
+    eventDeSerialization.getProducedType
+    stringSerialization.serialize("test", System.currentTimeMillis())
+    stringDeSerialization.isEndOfStream("") should be(false)
+    val map = new util.HashMap[String, AnyRef]()
+    map.put("country_code", "IN")
+    map.put("country", "INDIA")
+    mapSerialization.serialize(map, System.currentTimeMillis())
+  }
 }
 
 class Event(eventMap: util.Map[String, Any]) extends Events(eventMap) {
---
+++ data-pipeline-flink/dp-core/src/test/scala/org/sunbird/spec/TelemetryEventReaderSpec.scala
@@ -0,0 +1,67 @@
+package org.sunbird.spec
+
+import java.util
+
+import com.google.gson.Gson
+import org.scalatest.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+import org.sunbird.dp.domain.EventsPath
+import org.sunbird.dp.reader.Telemetry
+import org.sunbird.fixture.EventFixture
+
+class TelemetryEventReaderSpec extends BaseSpec with Matchers with MockitoSugar {
+  val gson = new Gson()
+
+  "Telemetry Reader" should "Able to read the particular key from the event" in {
+
+    val telemetryEvent = new Event(gson.fromJson(EventFixture.SAMPLE_EVENT_1, new util.LinkedHashMap[String, Any]().getClass))
+    telemetryEvent.eid() should be("INTERACT")
+    telemetryEvent.mid() should be("321a6f0c-10c6-4cdc-9893-207bb64fea50")
+    telemetryEvent.did() should be("758e054a400f20f7677f2def76427dc13ad1f837")
+    telemetryEvent.pid() should be("sunbird.app")
+    telemetryEvent.producerId() should be("prod.sunbird.portal")
+    telemetryEvent.producerPid() should be("sunbird.app")
+    telemetryEvent.actorId() should be("bc3be7ae-ad2b-4dee-ac4c-220c7db146b2")
+    telemetryEvent.actorType() should be("User")
+    telemetryEvent.flags() should be(null)
+    telemetryEvent.version() should be("3.0")
+    telemetryEvent.ets() should be(1579143065071L)
+    telemetryEvent.channel() should be("505c7c48ac6dc1edc9b08f21db5a571d")
+    telemetryEvent.objectID() should be("do_9574")
+    telemetryEvent.objectType() should be("content")
+    telemetryEvent.edataType() should be("OTHER")
+    telemetryEvent.edataItems() should be(null)
+    telemetryEvent.getFlags() should be(null)
+    telemetryEvent.getTimeStamp() should be("2020-01-21T00:02:54.098Z")
+  }
+
+  it should "Able to read the default, nested and update the particular key and value" in {
+    val eventMap = gson.fromJson(EventFixture.SAMPLE_EVENT_2, new util.LinkedHashMap[String, Any]().getClass)
+    val telemetryEvent = new Event(eventMap)
+    // read
+    telemetryEvent.edataItems() should not be (null)
+    telemetryEvent.edataItems().size() should be(3)
+
+    // Update
+    val telemetryReader: Telemetry = new Telemetry(eventMap)
+    telemetryReader.addFieldIfAbsent("flags", new util.HashMap[String, Boolean])
+    telemetryReader.add(s"flags.testflag", false)
+    val flags = telemetryReader.read[util.HashMap[String, Boolean]](EventsPath.FLAGS_PATH).getOrElse(null)
+    flags.get("testflag") should be(false)
+
+    // Nested
+    val producerPid = telemetryReader.read[String](EventsPath.CONTEXT_P_DATA_PID_PATH).getOrElse(null)
+    producerPid should not be(null)
+    producerPid should be("sunbird.app")
+  }
+
+  it should "Not fail when the particular key/nested key or value is not found" in {
+    val eventMap = gson.fromJson(EventFixture.SAMPLE_EVENT_2, new util.LinkedHashMap[String, Any]().getClass)
+    val telemetryReader: Telemetry = new Telemetry(eventMap)
+    val actorName = telemetryReader.read[String]("actor.id.name").getOrElse(null)
+    val userKey = telemetryReader.read[String]("user").getOrElse(null)
+    actorName should be(null)
+    userKey should be(null)
+  }
+
+}
