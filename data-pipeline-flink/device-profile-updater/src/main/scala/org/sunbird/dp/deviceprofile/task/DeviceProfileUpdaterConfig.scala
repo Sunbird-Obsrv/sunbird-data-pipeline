7d4796f969554eb0bfea4e57f88eb63bb43cdb6a
Manjunath Davanam <manjunathd@ilimi.in>
2020-04-21 17:14:13 +0530

    Issue SC-1732 feat:  Postgres connection util and testcase for Postgres connection class

--- data-pipeline-flink/device-profile-updater/src/main/scala/org/sunbird/dp/deviceprofile/task/DeviceProfileUpdaterConfig.scala
+++ data-pipeline-flink/device-profile-updater/src/main/scala/org/sunbird/dp/deviceprofile/task/DeviceProfileUpdaterConfig.scala
@@ -13,38 +13,18 @@ class DeviceProfileUpdaterConfig(override val config: Config) extends BaseJobCon
   private val serialVersionUID = 2905979434303791379L
 
   implicit val mapTypeInfo: TypeInformation[util.Map[String, AnyRef]] = TypeExtractor.getForClass(classOf[util.Map[String, AnyRef]])
-  implicit val stringTypeInfo: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
 
-  val dedupStore: Int = config.getInt("redis.database.duplicationstore.id")
-  val cacheExpirySeconds: Int = config.getInt("redis.database.key.expiry.seconds")
+  val deviceDbStore: Int = config.getInt("redis.database.devicestore.id")
 
   // Kafka Topics Configuration
   val kafkaInputTopic: String = config.getString("kafka.input.topic")
-  val kafkaSuccessTopic: String = config.getString("kafka.output.success.topic")
-  val kafkaDuplicateTopic: String = config.getString("kafka.output.duplicate.topic")
-  val kafkaFailedTopic: String = config.getString("kafka.output.failed.topic")
-  val eventMaxSize: Long = config.getLong("kafka.event.max.size")
 
-  val deDupParallelism: Int = config.getInt("task.dedup.parallelism")
-  val extractionParallelism: Int = config.getInt("task.extraction.parallelism")
-  
-  val UNIQUE_EVENTS_OUTPUT_TAG = "unique-events"
-  val RAW_EVENTS_OUTPUT_TAG = "raw-events"
-  val FAILED_EVENTS_OUTPUT_TAG = "failed-events"
-  val LOG_EVENTS_OUTPUT_TAG = "log-events"
-  val DUPLICATE_EVENTS_OUTPUT_TAG = "duplicate-events"
+  val deviceProfileParallelism: Int = config.getInt("task.deviceprofile.parallelism")
 
   // Metric List
-  val successEventCount = "success-event-count"
-  val failedEventCount = "failed-event-count"
-  val auditEventCount = "audit-event-count"
-  val totalBatchEventCount = "batch-event-count"
-  
-  val rawEventsOutputTag: OutputTag[util.Map[String, AnyRef]] = OutputTag[util.Map[String, AnyRef]](RAW_EVENTS_OUTPUT_TAG)
-  val failedEventsOutputTag: OutputTag[util.Map[String, AnyRef]] = OutputTag[util.Map[String, AnyRef]](FAILED_EVENTS_OUTPUT_TAG)
-  val logEventsOutputTag: OutputTag[util.Map[String, AnyRef]] = OutputTag[util.Map[String, AnyRef]](LOG_EVENTS_OUTPUT_TAG)
+  val deviceDbHitCount = "success-event-count"
+  val cacheHitCount = "failed-event-count"
 
-  val duplicateEventOutputTag: OutputTag[util.Map[String, AnyRef]] = OutputTag[util.Map[String, AnyRef]](id = DUPLICATE_EVENTS_OUTPUT_TAG)
-  val uniqueEventOutputTag: OutputTag[util.Map[String, AnyRef]] = OutputTag[util.Map[String, AnyRef]](id = UNIQUE_EVENTS_OUTPUT_TAG)
+  //val uniqueEventOutputTag: OutputTag[util.Map[String, AnyRef]] = OutputTag[util.Map[String, AnyRef]](id = UNIQUE_EVENTS_OUTPUT_TAG)
 
 }
