9e575cb6f5802f600ad9dc5d3228ae6f2d90fcc5
Manjunath Davanam <manjunathd@ilimi.in>
2020-04-22 18:48:05 +0530

    Issue SC-1732 feat: Postgres connection testcase

--- data-pipeline-flink/dp-core/src/test/scala/org/sunbird/spec/PostgresConnectSpec.scala
+++ data-pipeline-flink/dp-core/src/test/scala/org/sunbird/spec/PostgresConnectSpec.scala
@@ -2,6 +2,7 @@ package org.sunbird.spec
 
 import com.typesafe.config.{Config, ConfigFactory}
 import org.junit.Assert.{assertEquals, assertNotNull}
+import org.postgresql.util.PSQLException
 import org.scalatest.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 import org.sunbird.dp.core.util.{PostgresConnect, PostgresConnectionConfig}
@@ -27,7 +28,7 @@ class PostgresConnectSpec extends BaseSpec with Matchers with MockitoSugar {
     postgresConnect.execute("INSERT INTO device_table(id,channel)  VALUES('12345','custchannel');")
 
     val st = connection.createStatement
-    val rs = st.executeQuery("SELECT * FROM device_table where id='12345';")
+    val rs = postgresConnect.executeQuery("SELECT * FROM device_table where id='12345';")
     while ( {
       rs.next
     }) {
@@ -37,9 +38,21 @@ class PostgresConnectSpec extends BaseSpec with Matchers with MockitoSugar {
 
     val resetConnection = postgresConnect.resetConnection
     assertNotNull(resetConnection)
-
     postgresConnect.closeConnection()
-
+  }
+  it should "Re Connect while executing query if the the postgres connection is off " in {
+    val postgresConfig = PostgresConnectionConfig(
+      user = config.getString("postgres.user"),
+      password = config.getString("postgres.password"),
+      database = "postgres",
+      host = config.getString("postgres.host"),
+      port = config.getInt("postgres.port"),
+      maxConnections = config.getInt("postgres.maxConnections")
+    )
+    val postgresConnect = new PostgresConnect(postgresConfig)
+    val connection = postgresConnect.getConnection
+    connection.close()
+    postgresConnect.execute("CREATE TABLE device_table3(id text PRIMARY KEY, channel text);")
   }
 
 }
