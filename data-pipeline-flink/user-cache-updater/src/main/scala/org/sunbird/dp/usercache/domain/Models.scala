285a9623d76701d42f9252d700941171c3bfc264
Manjunath Davanam <manjunathd@ilimi.in>
2020-04-24 14:13:22 +0530

    Issue SC-1733 feat:UserCache Updater flink job setup

---
+++ data-pipeline-flink/user-cache-updater/src/main/scala/org/sunbird/dp/usercache/domain/Models.scala
@@ -0,0 +1,6 @@
+package org.sunbird.dp.denorm.domain
+
+import java.util
+
+import com.google.gson.Gson
+
---
+++ data-pipeline-flink/user-cache-updater/src/main/scala/org/sunbird/dp/usercache/task/UserCacheUpdaterStreamTask.scala
@@ -0,0 +1,38 @@
+package org.sunbird.dp.denorm.task
+
+import com.typesafe.config.ConfigFactory
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.java.typeutils.TypeExtractor
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.sunbird.dp.core.job.FlinkKafkaConnector
+import org.sunbird.dp.core.util.FlinkUtil
+import org.sunbird.dp.denorm.domain.Event
+
+class UserCacheUpdaterStreamTask(config: DenormalizationConfig, kafkaConnector: FlinkKafkaConnector) {
+
+  private val serialVersionUID = -7729362727131516112L
+
+  def process(): Unit = {
+    
+    implicit val env: StreamExecutionEnvironment = FlinkUtil.getExecutionContext(config)
+    implicit val eventTypeInfo: TypeInformation[Event] = TypeExtractor.getForClass(classOf[Event])
+
+    val source = kafkaConnector.kafkaEventSource[Event](config.inputTopic)
+    val deviceDenormStream = env.addSource(source, "denorm-consumer").rebalance().process(new DeviceDenormFunction(config))
+    env.execute(config.jobName)
+  }
+
+}
+
+// $COVERAGE-OFF$ Disabling scoverage as the below code can only be invoked within flink cluster
+object UserCacheUpdaterStreamTask {
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigFactory.load("user-cache-updater.conf").withFallback(ConfigFactory.systemEnvironment())
+    val eConfig = new DenormalizationConfig(config)
+    val kafkaUtil = new FlinkKafkaConnector(eConfig)
+    val task = new UserCacheUpdaterStreamTask(eConfig, kafkaUtil)
+    task.process()
+  }
+}
+// $COVERAGE-ON$
