d6c4bfdff9b350dec60c425f2c7e034e659bdc10
Manjunath Davanam <manjunathd@ilimi.in>
2020-04-28 10:37:17 +0530

    Issue SC-1732 feat: cassandra testcase fixes

--- data-pipeline-flink/user-cache-updater/src/test/scala/org/sunbird/dp/spec/UserCacheUpdatetStreamTaskSpec.scala
+++ data-pipeline-flink/user-cache-updater/src/test/scala/org/sunbird/dp/spec/UserCacheUpdatetStreamTaskSpec.scala
@@ -144,6 +144,8 @@ class UserCacheUpdatetStreamTaskSpec extends BaseTestSpec {
     cassandraUtil.reconnect()
     val response = cassandraUtil.findOne("SELECT * FROM sunbird.location;")
     response should not be(null)
+    val upsert = cassandraUtil.findOne("SELECT * FROM sunbird.location;")
+    upsert should not be(null)
     cassandraUtil.getUDTType("sunbird","test") should not be(not)
   }
 
