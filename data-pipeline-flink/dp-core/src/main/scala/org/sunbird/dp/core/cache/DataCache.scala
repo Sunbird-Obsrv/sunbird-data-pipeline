bbdac7f1ac31aa4a19ea375968ecc19e332bbe0d
Manjunath Davanam <manjunathd@ilimi.in>
2020-05-11 08:25:18 +0530

    Issue #000 fix: Redis dp(0) issue fix

--- data-pipeline-flink/dp-core/src/main/scala/org/sunbird/dp/core/cache/DataCache.scala
+++ data-pipeline-flink/dp-core/src/main/scala/org/sunbird/dp/core/cache/DataCache.scala
@@ -23,7 +23,7 @@ class DataCache(val config: BaseJobConfig, val redisConnect: RedisConnect, val d
 
   def close() {
     this.redisConnection.close()
-    redisConnect.closePool()
+    redisConnection.close()
   }
 
   def hgetAllWithRetry(key: String): Map[String, String] = {
@@ -32,7 +32,7 @@ class DataCache(val config: BaseJobConfig, val redisConnect: RedisConnect, val d
     } catch {
       case ex: JedisException =>
         logger.error("Exception when retrieving data from redis cache", ex)
-        redisConnect.resetConnection()
+        redisConnection.close()
         this.redisConnection = redisConnect.getConnection(dbIndex)
         hgetAll(key)
     }
@@ -56,7 +56,7 @@ class DataCache(val config: BaseJobConfig, val redisConnect: RedisConnect, val d
     } catch {
       case ex: JedisException =>
         logger.error("Exception when retrieving data from redis cache", ex)
-        redisConnect.resetConnection()
+        redisConnection.close()
         this.redisConnection = redisConnect.getConnection(dbIndex)
         get(key)
     }
@@ -97,7 +97,7 @@ class DataCache(val config: BaseJobConfig, val redisConnect: RedisConnect, val d
       case ex: JedisException => {
         println("dataCache")
         logger.error("Exception when inserting data to redis cache", ex)
-        redisConnect.resetConnection()
+        redisConnection.close()
         this.redisConnection = redisConnect.getConnection(dbIndex)
         this.redisConnection.hmset(key, value)
       }
@@ -108,9 +108,9 @@ class DataCache(val config: BaseJobConfig, val redisConnect: RedisConnect, val d
     try {
       set(key, value);
     } catch {
-      case ex @ (_ : JedisConnectionException | _ : JedisException) =>
+      case ex@(_: JedisConnectionException | _: JedisException) =>
         logger.error("Exception when update data to redis cache", ex)
-        redisConnect.resetConnection();
+        redisConnection.close()
         this.redisConnection = redisConnect.getConnection(dbIndex);
         set(key, value)
     }
@@ -121,4 +121,5 @@ class DataCache(val config: BaseJobConfig, val redisConnect: RedisConnect, val d
   }
 
 }
+
 // $COVERAGE-ON$
