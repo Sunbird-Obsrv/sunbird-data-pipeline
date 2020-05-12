ab9fef5735134596c047a9352b9c7cb522170811
Anand <anandp@ilimi.in>
2020-05-12 17:30:33 +0530

    Issue #000 chore: Move common config to base-config.conf

--- data-pipeline-flink/user-cache-updater/src/main/scala/org/sunbird/dp/usercache/task/UserCacheUpdaterConfig.scala
+++ data-pipeline-flink/user-cache-updater/src/main/scala/org/sunbird/dp/usercache/task/UserCacheUpdaterConfig.scala
@@ -34,12 +34,12 @@ class UserCacheUpdaterConfig(override val config: Config) extends BaseJobConfig(
   // Redis
   val userStore: Int = config.getInt("redis.database.userstore.id")
 
-  // cassandra
-  val keySpace: String = config.getString("cassandra.keyspace")
-  val locationTable: String = config.getString("cassandra.table.location")
-  val userTable: String = config.getString("cassandra.table.user")
-  val cassandraHost: String =  config.getString("cassandra.host")
-  val cassandraPort: Int =  config.getInt("cassandra.port")
+  // lms-cassandra
+  val keySpace: String = config.getString("lms-cassandra.keyspace")
+  val locationTable: String = config.getString("lms-cassandra.table.location")
+  val userTable: String = config.getString("lms-cassandra.table.user")
+  val cassandraHost: String =  config.getString("lms-cassandra.host")
+  val cassandraPort: Int =  config.getInt("lms-cassandra.port")
 
   val userCacheParallelism: Int = config.getInt("task.usercache.updater.parallelism")
 
