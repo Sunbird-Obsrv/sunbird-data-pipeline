72b7498bb1782deda0e5490ef1af6675447af3b1
Manjunath Davanam <manjunathd@ilimi.in>
2020-07-08 17:32:05 +0530

    Issue SC-1810 fix: Migration script to populate the user cache data

--- druid/adhoc-jobs/src/main/scala/org/ekstep/analytics/jobs/CassandraRedisIndexer.scala
+++ druid/adhoc-jobs/src/main/scala/org/ekstep/analytics/jobs/CassandraRedisIndexer.scala
@@ -4,40 +4,76 @@ import com.typesafe.config.{Config, ConfigFactory}
 import org.ekstep.analytics.util.JSONUtils
 import com.datastax.spark.connector._
 import com.redislabs.provider.redis._
+import org.apache.commons.lang.{StringEscapeUtils, StringUtils}
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 
 object CassandraRedisIndexer {
 
-   private val config: Config = ConfigFactory.load
+  private val config: Config = ConfigFactory.load
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        val conf = new SparkConf()
-            .setAppName("CassandraToRedisIndexer")
-            .setMaster("local[*]")
-            // Cassandra settings
-            .set("spark.cassandra.connection.host", config.getString("spark.cassandra.connection.host"))
-            // redis settings
-            .set("spark.redis.host", config.getString("redis.host"))
-            .set("spark.redis.port", config.getString("redis.port"))
-            .set("spark.redis.db", config.getString("redis.user.database.index"))
-            .set("spark.redis.max.pipeline.size", config.getString("redis.max.pipeline.size"))
+    val isForAllUsers = true
+    val specificUserId = null
 
-        val sc = new SparkContext(conf)
+    val conf = new SparkConf()
+      .setAppName("CassandraToRedisIndexer")
+      .setMaster("local[*]")
+      // Cassandra settings
+      .set("spark.cassandra.connection.host", config.getString("spark.cassandra.connection.host"))
+      // redis settings
+      .set("spark.redis.host", config.getString("redis.host"))
+      .set("spark.redis.port", config.getString("redis.port"))
+      .set("spark.redis.db", config.getString("redis.user.database.index"))
+      .set("spark.redis.max.pipeline.size", config.getString("redis.max.pipeline.size"))
 
-        val userKeyspace = config.getString("cassandra.user.keyspace")
-        val userTableName = config.getString("cassandra.user.table")
-        val redisKeyProperty: String = config.getString("redis.user.index.source.key")
+    val sc = new SparkContext(conf)
 
-        val userData = sc.cassandraTable(userKeyspace, userTableName)
-        val userMap = userData.map(f => f.toMap)
-        val mappedData = userMap.map { obj =>
-            (obj.getOrElse(redisKeyProperty, "").asInstanceOf[String] , JSONUtils.serialize(obj))
-        }
-        sc.toRedisKV(mappedData)
+    val userKeyspace = config.getString("cassandra.user.keyspace")
+    val userTableName = config.getString("cassandra.user.table")
+    val redisKeyProperty: String = config.getString("redis.user.index.source.key")
+    val insertionType: String = config.getString("insertion.type")
+
+    def seqOfAnyToSeqString(param: Seq[Any]): Seq[(String, String)]
+    = {
+      param.map {
+        case (x, y) => (x.toString, y.toString)
+      }
     }
 
+    def getFilteredUserRecords(usersData: RDD[Map[String, Any]]): RDD[Map[String, Any]] = {
+      if (isForAllUsers) {
+        usersData
+      } else if (null != specificUserId) {
+        usersData.filter(user => StringUtils.equalsIgnoreCase(user.getOrElse("id", "").asInstanceOf[String], specificUserId))
+      } else {
+        null
+      }
+    }
 
+    val usersData = getFilteredUserRecords(sc.cassandraTable(userKeyspace, userTableName).map(f => f.toMap))
+
+    if (StringUtils.equalsIgnoreCase(insertionType, "hashmap")) {
+      val mappedData = usersData.map { obj =>
+        (obj.getOrElse(redisKeyProperty, "").asInstanceOf[String], obj)
+      }
+      mappedData.collect().foreach(record => {
+        val filteredData = record._2.toSeq.filterNot { case (_, y) => (
+          y.isInstanceOf[String] &&
+            y.asInstanceOf[String].forall(_.isWhitespace)
+          ) || y == null
+        }
+        val userRdd = sc.parallelize(seqOfAnyToSeqString(filteredData))
+        sc.toRedisHASH(userRdd, record._1)
+      })
+    } else {
+      val mappedData = usersData.map { obj =>
+        (obj.getOrElse(redisKeyProperty, "").asInstanceOf[String], JSONUtils.serialize(obj))
+      }
+      sc.toRedisKV(mappedData)
+    }
+  }
 }
