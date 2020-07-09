50e2dcb75557bf0a1484ca78853888550d536f66
Manjunath Davanam <manjunathd@ilimi.in>
2020-07-09 15:07:06 +0530

    Issue SC-1810 fix: migration script issue fix

--- druid/adhoc-jobs/src/main/scala/org/ekstep/analytics/jobs/CassandraRedisIndexer.scala
+++ druid/adhoc-jobs/src/main/scala/org/ekstep/analytics/jobs/CassandraRedisIndexer.scala
@@ -1,14 +1,14 @@
 package org.ekstep.analytics.jobs
 
-import com.typesafe.config.{Config, ConfigFactory}
-import org.ekstep.analytics.util.JSONUtils
+import java.text.SimpleDateFormat
+
 import com.datastax.spark.connector._
-import com.google.gson.Gson
 import com.redislabs.provider.redis._
-import org.apache.commons.lang.{StringEscapeUtils, StringUtils}
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.commons.lang.StringUtils
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.ekstep.analytics.util.JSONUtils
 
 
 object CassandraRedisIndexer {
@@ -17,14 +17,9 @@ object CassandraRedisIndexer {
 
   def main(args: Array[String]): Unit = {
 
-    val isForAllUsers = "true"
-    val specificUserId = null
-    val specificDate = null
-    val insertionType = "hashmap"
-    println("arg0" + args(0))
-    println("arg1" + args(1))
-    println("arg2" + args(2))
-    println("arg3" + args(3))
+    val isForAllUsers = args(0) // true/false
+    val specificUserId = args(1) // userid
+    val fromSpecificDate = args(2) // date in YYYY-MM-DD format
 
     val conf = new SparkConf()
       .setAppName("CassandraToRedisIndexer")
@@ -41,43 +36,42 @@ object CassandraRedisIndexer {
 
     val userKeyspace = config.getString("cassandra.user.keyspace")
     val userTableName = config.getString("cassandra.user.table")
-    val redisKeyProperty: String = config.getString("redis.user.index.source.key")
-    //val insertionType: String = config.getString("insertion.type")
+    val redisKeyProperty: String = config.getString("redis.user.index.source.key") // userid
 
     def seqOfAnyToSeqString(param: Seq[AnyRef]): Seq[(String, String)]
     = {
       param.map {
-        case (x, y) => (x.toString, if(!y.isInstanceOf[String]) JSONUtils.serialize(y.asInstanceOf[AnyRef]) else  y.asInstanceOf[String])
+        case (x, y) => (x.toString, if (!y.isInstanceOf[String]) JSONUtils.serialize(y.asInstanceOf[AnyRef]) else y.asInstanceOf[String])
       }
     }
 
-    def getFilteredUserRecords(usersData: RDD[Map[String, AnyRef]]): RDD[Map[String, AnyRef]] = {
+    val dtf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSSZ")
+    val dtf2 = new SimpleDateFormat("yyyy-MM-dd")
+
+    def getFilteredUserRecords(usersData: RDD[Map[String, Any]]): RDD[Map[String, Any]] = {
       if (StringUtils.equalsIgnoreCase(isForAllUsers, "true")) {
         usersData
       } else if (null != specificUserId) {
         usersData.filter(user => StringUtils.equalsIgnoreCase(user.getOrElse(redisKeyProperty, "").asInstanceOf[String], specificUserId))
+      } else if (null != fromSpecificDate) {
+        println(s"Fetching all the user records from this specific date:$fromSpecificDate ")
+        usersData.filter(user => {
+          dtf1.parse(user.getOrElse("updateddate", null).asInstanceOf[String]).after(dtf2.parse(fromSpecificDate))
+        })
       } else {
         println("Data is not fetching from the table since input is invalid")
         null
       }
     }
 
-    val usersData = getFilteredUserRecords(sc.cassandraTable(userKeyspace, userTableName)).map(f => f.toMap.asInstanceOf[Map[String, AnyRef]])
-    println("usersData" + usersData.count())
-
-    if (StringUtils.equalsIgnoreCase(insertionType, "hashmap")) {
-      val mappedData = usersData.map { obj =>
-        (obj.getOrElse(redisKeyProperty, "").asInstanceOf[String], obj)
-      }
-      mappedData.collect().foreach(record => {val filteredData = record._2.toSeq.filterNot { case (_, y) => (y.isInstanceOf[String] && y.asInstanceOf[String].forall(_.isWhitespace)) || y == null}
-        val userRdd = sc.parallelize(seqOfAnyToSeqString(filteredData))
-        sc.toRedisHASH(userRdd, record._1)
-      })
-    } else if(StringUtils.equalsIgnoreCase(insertionType, "json")) {
-      val mappedData = usersData.map { obj =>
-        (obj.getOrElse(redisKeyProperty, "").asInstanceOf[String], JSONUtils.serialize(obj))
-      }
-      sc.toRedisKV(mappedData)
+    val usersData = getFilteredUserRecords(sc.cassandraTable(userKeyspace, userTableName).map(f => f.toMap))
+    val mappedData = usersData.map { obj =>
+      (obj.getOrElse(redisKeyProperty, "").asInstanceOf[String], obj)
     }
+    mappedData.collect().foreach(record => {
+      val filteredData = record._2.toSeq.filterNot { case (_, y) => (y.isInstanceOf[String] && y.asInstanceOf[String].forall(_.isWhitespace)) || y == null }
+      val userRdd = sc.parallelize(seqOfAnyToSeqString(filteredData))
+      sc.toRedisHASH(userRdd, record._1)
+    })
   }
 }
