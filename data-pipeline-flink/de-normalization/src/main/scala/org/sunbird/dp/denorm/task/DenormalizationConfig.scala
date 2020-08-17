0ed352eb167f6859d05d734dd598f2c0933e2bdf
Utkarsha Kapoor <utkit14@gmail.com>
2020-08-17 12:15:51 +0530

    Issue #0000: remove from test-2.conf

--- data-pipeline-flink/de-normalization/src/main/scala/org/sunbird/dp/denorm/task/DenormalizationConfig.scala
+++ data-pipeline-flink/de-normalization/src/main/scala/org/sunbird/dp/denorm/task/DenormalizationConfig.scala
@@ -35,7 +35,7 @@ class DenormalizationConfig(override val config: Config, jobName: String) extend
 
   //config version
   val version: String = config.getString("version")
-  
+
   val deviceFields = List("country_code", "country", "state_code", "state", "city", "district_custom", "state_code_custom",
     "state_custom", "user_declared_state", "user_declared_district", "devicespec", "firstaccess")
   val contentFields = List("name", "objectType", "contentType", "mediaType", "language", "medium", "mimeType", "createdBy",
