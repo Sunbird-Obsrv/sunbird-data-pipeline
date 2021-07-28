6c2554aa1b1537dc72460b99f0ae09d1800f4b13
Utkarsha Kapoor <31096446+utk14@users.noreply.github.com>
2021-07-28 18:25:34 +0530

    Revert "Issue #SB-25755: Loggers added for denorm job to verify AUDIT events"

--- data-pipeline-flink/de-normalization/src/main/scala/org/sunbird/dp/denorm/type/ContentDenormalization.scala
+++ data-pipeline-flink/de-normalization/src/main/scala/org/sunbird/dp/denorm/type/ContentDenormalization.scala
@@ -1,6 +1,5 @@
 package org.sunbird.dp.denorm.`type`
 
-import org.slf4j.LoggerFactory
 import org.sunbird.dp.core.domain.EventsPath
 import org.sunbird.dp.core.job.Metrics
 import org.sunbird.dp.denorm.domain.Event
@@ -9,13 +8,10 @@ import org.sunbird.dp.denorm.util.CacheResponseData
 
 class ContentDenormalization(config: DenormalizationConfig) {
 
-  private[this] val logger = LoggerFactory.getLogger(classOf[ContentDenormalization])
-
   def denormalize(event: Event, cacheData: CacheResponseData, metrics: Metrics) = {
     val objectType = event.objectType()
     val objectId = event.objectID()
     if (event.isValidEventForContentDenorm(config, objectId, objectType, event.eid())) {
-      logger.info(s"ContentDenormalization::Event Valid For Content Denorm having mid:${event.mid()} in event: " + event)
       metrics.incCounter(config.contentTotal)
       val contentData = cacheData.content.map(f => {
         (f._1.toLowerCase().replace("_", ""), f._2)
@@ -39,8 +35,6 @@ class ContentDenormalization(config: DenormalizationConfig) {
           (f._1.toLowerCase().replace("_", ""), f._2)
         }))
       }
-    } else {
-      logger.info(s"ContentDenormalization::Event Not Valid For Content Denorm having mid:${event.mid()} in event: " + event)
     }
   }
 
